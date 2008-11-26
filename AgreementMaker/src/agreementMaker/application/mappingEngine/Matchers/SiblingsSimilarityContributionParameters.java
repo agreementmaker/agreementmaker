338bc9203d59020a01c90cc33665688265654926
Cosmin Stroe <cstroe@gmail.com>
2008-11-26 07:53:34 +0000

    Sibbling Similarity Contribution Matcher.  First commit.

---
+++ AgreementMaker/src/agreementMaker/application/mappingEngine/Matchers/SiblingsSimilarityContributionParameters.java
@@ -0,0 +1,9 @@
+package agreementMaker.application.mappingEngine.Matchers;
+
+import agreementMaker.application.mappingEngine.AbstractParameters;
+
+public class SiblingsSimilarityContributionParameters extends AbstractParameters {
+
+	public double MCP = 0.75d;
+	
+}
