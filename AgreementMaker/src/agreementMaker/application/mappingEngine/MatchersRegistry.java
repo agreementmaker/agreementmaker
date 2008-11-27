cd2e723257d58852daa6aa0fd9628e4ba693ace6
Cosmin Stroe <cstroe@gmail.com>
2008-11-27 04:32:02 +0000

    MatchersRegistry initial import.

--- AgreementMaker/src/agreementMaker/application/mappingEngine/MatchersRegistry.java
+++ AgreementMaker/src/agreementMaker/application/mappingEngine/MatchersRegistry.java
@@ -1,29 +1,46 @@
 package agreementMaker.application.mappingEngine;
 
-import agreementMaker.application.mappingEngine.Matchers.BaseSimilarityMatcher;
-import agreementMaker.application.mappingEngine.Matchers.DescendantsSimilarityInheritanceMatcher;
-import agreementMaker.application.mappingEngine.Matchers.SiblingsSimilarityContributionMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.AllOneMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.AllZeroMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.CopyMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.EqualsMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.RandomMatcher;
-import agreementMaker.application.mappingEngine.fakeMatchers.UserManualMatcher;
+import agreementMaker.application.mappingEngine.baseSimilarity.BaseSimilarityMatcher;
+import agreementMaker.application.mappingEngine.dsi.DescendantsSimilarityInheritanceMatcher;
+import agreementMaker.application.mappingEngine.manualMatcher.EmptyMatcher;
+import agreementMaker.application.mappingEngine.manualMatcher.UserManualMatcher;
+import agreementMaker.application.mappingEngine.ssc.SiblingsSimilarityContributionMatcher;
+import agreementMaker.application.mappingEngine.testMatchers.AllOneMatcher;
+import agreementMaker.application.mappingEngine.testMatchers.CopyMatcher;
+import agreementMaker.application.mappingEngine.testMatchers.EqualsMatcher;
+import agreementMaker.application.mappingEngine.testMatchers.RandomMatcher;
 
 /**
  * Enum for keeping the current list of matchers in the system, and their class references
  */
 public enum MatchersRegistry {
+	
+	/**
+	 * This is where you add your own MATCHER.
+	 * 
+	 * To add your matcher, add a definition to the enum, using this format
+	 * 
+	 * 		EnumName	( "Short Name", MatcherClass.class )
+	 * 
+	 * For example, to add MySuperMatcher, you would add something like this (assuming the class name is MySuperMatcher):
+	 *  
+	 * 		SuperMatcher   ( "My Super Matcher", MySuperMatcher.class ),
+	 * 
+	 * And so, if your matcher is has no code errors, it will be incorporated into the AgreementMaker.  - Cosmin
+	 */
+	
 	UserManual		( "User Manual Matching", UserManualMatcher.class, false),
 	Equals 			( "Local Name Equivalence Comparison", EqualsMatcher.class ),
 	Random 			( "Random Similarity Matcher", RandomMatcher.class ),
 	AllOne 			( "All ONE Similarities", AllOneMatcher.class ),
-	AllZero			( "All ZERO Similarities", AllZeroMatcher.class ),
+	EmptyMatcher	( "Empty Matcher", EmptyMatcher.class ),
 	Copy			( "Copy Matcher", CopyMatcher.class ),
 	BaseSimilarity	( "Base Similarity", BaseSimilarityMatcher.class ),
 	DSI				( "Descendant's Similarity Inheritance (DSI)", DescendantsSimilarityInheritanceMatcher.class ),
 	SSC				( "Sibling's Similarity Contribution (SSC)", SiblingsSimilarityContributionMatcher.class );
 	
+	
+	/* Don't change anything below this line .. unless you intend to. */
 	private boolean showInControlPanel;
 	private String name;
 	private String className;
