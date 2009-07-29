858229e1df3034041a302d00aedbe4d289bc576d
Ulas Cagdas Keles <ukeles2@uic.edu>
2009-07-29 22:06:51 +0000

    *** empty log message ***

---
+++ AgreementMaker/src/am/application/mappingEngine/LexicalMatcher/LexicalMatcherParameters.java
@@ -0,0 +1,9 @@
+package am.application.mappingEngine.LexicalMatcher;
+
+import am.application.mappingEngine.AbstractParameters;
+
+public class LexicalMatcherParameters extends AbstractParameters{
+
+	public boolean useDictionary = false;
+	
+}
