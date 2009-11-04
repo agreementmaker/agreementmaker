e9e3c493d98e3d8edb6d0b81035fd1a79b052a3d
Cosmin Stroe <cstroe@gmail.com>
2009-11-04 21:41:09 +0000

    New function for getting a String list.

--- AgreementMaker/src/am/Utility.java
+++ AgreementMaker/src/am/Utility.java
@@ -21,7 +21,7 @@ public class Utility {
 	
 	//USED by the tuning alg.
 	//we can't generate this array with a for because the 0.35 + 0.05 = 0.39 in java because of double representations, exclude <10 and >90
-	public final static double[] STEPFIVE = {0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90};
+	public final static double[] STEPFIVE = {0.0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.0};
 	public final static String[] STEPFIVE_INT = {"5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60", "65", "70", "75", "80", "85", "90" };
 	
 	//**************************************************USER INTERFACE UTILITIES***********************************************************
@@ -51,6 +51,26 @@ public class Utility {
 			Double k = new Double( Double.parseDouble(Integer.toString(i)) / 100 );
 			current =  k.toString() ;
 			s[j] = current;
+		}
+		return s;
+	}
+	
+	// return a string array
+	public static String[] getPercentDecimal( double start, double eachStep, int numSteps ) {
+		
+		String[] s = new String[numSteps];
+		for(int i = 0; i < s.length; i++) {
+			s[i] = Double.toString(start + i*eachStep);
+		}
+		return s;
+	}
+	
+	// return a double array with regular entries
+	public static double[] getDoubleArray( double start, double eachStep, int numSteps ) {
+		
+		double[] s = new double[numSteps];
+		for(int i = 0; i < s.length; i++) {
+			s[i] = start + i*eachStep;
 		}
 		return s;
 	}
