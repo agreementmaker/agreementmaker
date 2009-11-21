d30034c5a4344c6dcf68150cc92f4e1f8ef8b0ea
Cosmin Stroe <cstroe@gmail.com>
2009-11-21 15:42:16 +0000

    Solve visibility issues.

--- AgreementMaker/src/am/userInterface/canvas2/nodes/LegacyMapping.java
+++ AgreementMaker/src/am/userInterface/canvas2/nodes/LegacyMapping.java
@@ -1,6 +1,9 @@
 package am.userInterface.canvas2.nodes;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 import am.userInterface.canvas2.graphical.GraphicalData;
 import am.userInterface.canvas2.graphical.MappingData;
@@ -9,6 +12,8 @@ import am.userInterface.canvas2.utility.Canvas2Edge;
 import am.userInterface.canvas2.utility.Canvas2Vertex;
 
 public class LegacyMapping extends Canvas2Edge {
+
+	Line2D.Double line; // the line formed by the mapping
 	
 	public LegacyMapping(Canvas2Vertex orig, Canvas2Vertex dest, GraphicalData o, int matcherID, String label ) {
 		super(orig, dest, o);
@@ -38,8 +43,24 @@ public class LegacyMapping extends Canvas2Edge {
 		
 		setObject( data );
 		
+		Point2D.Double startPoint = new Point2D.Double(d.x, d.y);
+		Point2D.Double endPoint =   new Point2D.Double(d.x+d.width, d.y+d.height);
+		
+		line = new Line2D.Double( startPoint, endPoint );
 		
 	}
+	
+	// Used when the line endpoints have changed.
+	@Override
+	public void updateBounds( int x, int y, int width, int height ) { 
+		super.updateBounds(x, y, width, height);
+		
+		// recreate the line
+		Point2D.Double startPoint = new Point2D.Double(d.x, d.y);
+		Point2D.Double endPoint =   new Point2D.Double(d.x+d.width, d.y+d.height);
+		
+		line = new Line2D.Double( startPoint, endPoint );
+	};
 	
 	@Override
 	public void draw(Graphics g ) {
@@ -48,5 +69,19 @@ public class LegacyMapping extends Canvas2Edge {
 		if( ((MappingData)d).label != null )
 			g.drawString( ((MappingData)d).label, d.x+d.width/2, d.y+d.height/2);
 	}
+	
+	@Override
+	public boolean isVisible(Rectangle bounds) {
+		//if( graph != null ) { if( !graph.visible ) return false; }  // if the graph isn't visible, any of its elements aren't visible either
+		if( d == null ) return false; // no GraphicalData -> no visibility
+		
+		if( !d.visible ) return false;  // if our node was set to be invisible, so no need to check bounds
+		
+		if( bounds.getBounds2D().intersectsLine(line) ) return true; // if the line formed by the mapping intersects the viewport rectangle
+		
+		return false;
+		
+	}
+	
 
 }
