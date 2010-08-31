930c83a70c21c704dd7062c85250a1c6b813869e
Cosmin Stroe <cstroe@gmail.com>
2010-08-31 23:57:21 +0000

    StringAnalyzer - currently a mystery!

---
+++ AgreementMaker/src/am/app/mappingEngine/LexicalMatcherJAWS/StringAnalyzer.java
@@ -0,0 +1,14 @@
+package am.app.mappingEngine.LexicalMatcherJAWS;
+
+public class StringAnalyzer {
+
+	public static enum StringProperties {
+		
+	}
+	
+	
+	public StringAnalyzer( String input ) {
+		
+	}
+	
+}
