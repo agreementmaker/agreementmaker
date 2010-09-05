38fd29cf9c1cc76551385590e21fa5e02f10c1c8
Cosmin Stroe <cstroe@gmail.com>
2010-09-05 05:08:10 +0000

    Needed a Pair class.

---
+++ AgreementMaker/src/am/utility/Pair.java
@@ -0,0 +1,18 @@
+package am.utility;
+
+public class Pair<E,V> {
+
+	private E left;
+	private V right;
+	
+	public Pair( E l, V r ) {
+		left = l;
+		right = r;
+	}
+	
+	public E getLeft() { return left; }
+	public V getRight() { return right; }
+	
+	public void setLeft(E l) { left = l; }
+	public void setRight(V r){ right = r;}
+}
