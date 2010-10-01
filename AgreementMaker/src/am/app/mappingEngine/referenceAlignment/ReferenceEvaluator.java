8ee55bdf6cda95c94c1e2dbb4e1d13d09307bfc1
Cosmin Stroe <cstroe@gmail.com>
2010-10-01 20:33:48 +0000

    Refactor.

--- AgreementMaker/src/am/app/mappingEngine/referenceAlignment/ReferenceEvaluator.java
+++ AgreementMaker/src/am/app/mappingEngine/referenceAlignment/ReferenceEvaluator.java
@@ -6,82 +6,90 @@ import am.app.mappingEngine.AlignmentSet;
 public class ReferenceEvaluator{
 
 	
-	final static double ALPHA = 1;
+	final static double ALPHA = 0.5d; // ALPHA can range from 0 to 1.  When ALPHA = 0, FM = PREC.  When ALPHA = 1, FM = REC. 
+								      // The higher ALPHA is, the more importance is given to PREC and less importance to REC. 
+									  // When ALPHA = 0.5, this is the harmonic mean of PREC and REC.
 	/**
 	 * Perform a reference evaluation.
-	 * @param as1 The alignment set of the matcher that must be evaluated.
-	 * @param as2 The reference alignment.
+	 * @param evaluationSet The alignment set of the matcher that must be evaluated.
+	 * @param referenceSet The reference alignment.
 	 * @return
 	 */
-    public static ReferenceEvaluationData compare(AlignmentSet as1, AlignmentSet as2)
+    public static ReferenceEvaluationData compare(AlignmentSet<?> evaluationSet, AlignmentSet<?> referenceSet)
     {
-        int found = 0; 
-        int exist = 0;
-        if(as1 != null) {
-        	found = as1.size();
+        int foundMappings = 0; 
+        int referenceMappings = 0;
+        if(evaluationSet != null) {
+        	foundMappings = evaluationSet.size();
         }
-        if (as2 != null) {
-            exist = as2.size();
+        if (referenceSet != null) {
+            referenceMappings = referenceSet.size();
         }
-        int correct = 0;
+        
         ReferenceEvaluationData result = new ReferenceEvaluationData();
-        AlignmentSet errorAlignments = new AlignmentSet();
-        AlignmentSet correctAlignments = new AlignmentSet();
-        AlignmentSet lostAlignments = new AlignmentSet();
-        for (int i = 0; i < found; i++) {
-            Alignment alignment1 = (Alignment) as1.getAlignment(i);
-            boolean flag = true;
-            for (int j = 0; j < exist; j++) {
-                Alignment alignment2 = (Alignment) as2.getAlignment(j);
-                if (alignment1.equals(alignment2)) {
-                    correct++;
-                    correctAlignments.addAlignment(alignment1);
-                    flag = false;
+        
+        AlignmentSet<Alignment> errorAlignments = new AlignmentSet<Alignment>();
+        AlignmentSet<Alignment> correctAlignments = new AlignmentSet<Alignment>();
+        AlignmentSet<Alignment> lostAlignments = new AlignmentSet<Alignment>();
+        
+        int correctMappings = 0;
+        // check all mappings for correctness
+        for (int i = 0; i < foundMappings; i++) {
+            Alignment evaluationMapping = evaluationSet.getAlignment(i);
+            boolean evaluationMappingIsWrong = true;
+            for (int j = 0; j < referenceMappings; j++) {
+                Alignment referenceMapping = referenceSet.getAlignment(j);
+                if (evaluationMapping.equals(referenceMapping)) {
+                    correctMappings++;
+                    correctAlignments.addAlignment(evaluationMapping);
+                    evaluationMappingIsWrong = false;
                     break;
                 }
             }
-            if (flag == true) {
-                errorAlignments.addAlignment(alignment1);
+            if (evaluationMappingIsWrong == true) {
+                errorAlignments.addAlignment(evaluationMapping);
             }
         }
-        for (int i = 0; i < exist; i++) {
-            Alignment alignment1 = as2.getAlignment(i);
-            boolean flag = true;
-            for (int j = 0; j < found; j++) {
-                Alignment alignment2 = as1.getAlignment(j);
-                if (alignment1.equals(alignment2)) {
-                    flag = false;
+        
+        for (int i = 0; i < referenceMappings; i++) {
+            Alignment referenceMapping = referenceSet.getAlignment(i);
+            boolean referenceMappingNotFound = true;
+            for (int j = 0; j < foundMappings; j++) {
+                Alignment evaluationMapping = evaluationSet.getAlignment(j);
+                if (referenceMapping.equals(evaluationMapping)) {
+                    referenceMappingNotFound = false;
                     break;
                 }
             }
-            if (flag == true) {
-                lostAlignments.addAlignment(alignment1);
+            if (referenceMappingNotFound == true) {
+                lostAlignments.addAlignment(referenceMapping);
             }
         }
         //System.out.println("Found: " + found + ", Exist: " + exist + ", Correct: " + correct);
         double prec;
-        if(found == 0) {
+        if(foundMappings == 0.0d) {
         	prec = 1;
         }
-        else prec = (double) correct / found;
+        else prec = (double) correctMappings / foundMappings;
         
         double rec;
-        if(exist == 0) {
+        if(referenceMappings == 0.0d) {
         	rec = 1;
         }
-        else rec = (double) correct / exist;
+        else rec = (double) correctMappings / referenceMappings;
         //System.out.println("Precision: " + prec + ", Recall: " + rec);
         // F-measure
         double fm;
-        if(prec + rec == 0) {
-        	fm = 0;
+        if(prec + rec == 0.0d) {
+        	fm = 0.0d;
         }
-        else  fm = (1 + ALPHA) * (prec * rec) / (ALPHA * prec + rec);
+        //else  fm = (1 + ALPHA) * (prec * rec) / (ALPHA * prec + rec);
+        else fm = (prec * rec) / ( ((1-ALPHA) * prec) + (ALPHA * rec) );  // from Ontology Matching book
         //System.out.println("F-Measure: " + fm);
 
-        result.setFound(found);
-        result.setExist(exist);
-        result.setCorrect(correct);
+        result.setFound(foundMappings);
+        result.setExist(referenceMappings);
+        result.setCorrect(correctMappings);
         result.setPrecision(prec);
         result.setRecall(rec);
         result.setFmeasure(fm);
