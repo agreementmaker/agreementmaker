c8928628bfc04f517aa9c6ae4923121817f3511a
Cosmin Stroe <cstroe@gmail.com>
2010-10-14 10:24:45 +0000

    Can save an AlignmentMatrix as CSV.

--- AgreementMaker/src/am/output/OutputController.java
+++ AgreementMaker/src/am/output/OutputController.java
@@ -3,10 +3,16 @@ package am.output;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
+import am.Utility;
 import am.app.Core;
 import am.app.mappingEngine.AbstractMatcher;
+import am.app.mappingEngine.Alignment;
+import am.app.mappingEngine.AlignmentMatrix;
+import am.utility.Capsule;
+import am.utility.Pair;
 /**
  * Q: What do the methods of this class do? - Cosmin. (Oct 13th, 2010)
  * A: They are responsible for saving the AlignmentSets in different formats. - Cosmin
@@ -43,7 +49,7 @@ public class OutputController {
 	public static void printDocumentOAEI(String name) throws Exception{
 		ArrayList<AbstractMatcher> list = Core.getInstance().getMatcherInstances();
 		AbstractMatcher matcher;
-		Core.getInstance();
+
 		//TO DO:
 		//May be multiple matchers.
 		int [] rowsIndex = Core.getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
@@ -57,7 +63,6 @@ public class OutputController {
 	// TODO: This function should differentiate between TXT, DOC, and XLS formats. (Or create separate print functions for each format)
 	public static void printDocument(String name) throws Exception{
 		
-		Core.getInstance();
 		int [] rowsIndex = Core.getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
 		
 		Date d = new Date();
@@ -86,6 +91,75 @@ public class OutputController {
 			result += matcher.getAlignmentsStrings();
 		}
 		return result;
+	}
+	
+	/**
+	 * Save an AlignmentMatrix to a file, as Comma Separated Values of the form: sourceIndex, targetIndex, similarity. 
+	 * @param matrix The matrix to be exported.
+	 * @param fullFileName The file to which the matrix will be saved.
+	 * @param doSort If true, the rows and columns will be sorted in order to put the highest similarities in one corner of the matrix.
+	 * @throws Exception If anything goes wrong, an exception is thrown.
+	 */
+	public static void saveMatrixAsCSV(AlignmentMatrix matrix, String fullFileName, boolean doSort, boolean doIsolines, boolean skipZeros) throws Exception {
+		
+		if( matrix == null ) throw new Exception("Cannot save a null AlignmentMatrix.");  
+
+		if( doSort ) {
+			ArrayList<Capsule<Integer>> rowList = new ArrayList<Capsule<Integer>>(); 
+			
+			for( int row = 0; row < matrix.getRows(); row++ ) {
+				Alignment[] rowMax = matrix.getRowMaxValues(row, 1);
+				rowList.add( new Capsule<Integer>( rowMax[0].getSimilarity() , new Integer(row) ));
+			}
+			
+			ArrayList<Capsule<Integer>> colList = new ArrayList<Capsule<Integer>>(); 
+			
+			for( int col = 0; col < matrix.getColumns(); col++ ) {
+				Alignment[] colMax = matrix.getColMaxValues(col, 1);
+				colList.add( new Capsule<Integer>( colMax[0].getSimilarity() , new Integer(col) ));
+			}
+			
+			Collections.sort(rowList);
+			Collections.sort(colList);
+			
+			
+			
+			FileOutputStream out = new FileOutputStream(fullFileName);
+			PrintStream p = new PrintStream( out );
+			
+			for( int row = 0; row < matrix.getRows(); row++ ) {
+				for( int col = 0; col < matrix.getColumns(); col++ ) {
+					if( skipZeros && colList.get(col).getPayload().intValue() == 0 ) continue;
+					p.println( row + "," + col + "," + 
+							Utility.roundDouble( matrix.getSimilarity( rowList.get(row).getPayload().intValue() , colList.get(col).getPayload().intValue()), 4 ) );
+				}
+				if( doIsolines ) p.println(""); // blank lines between scans
+			}
+			
+			p.close();
+			out.close();
+		} else {
+			FileOutputStream out = new FileOutputStream(fullFileName);
+			PrintStream p = new PrintStream( out );
+			
+			for( int row = 0; row < matrix.getRows(); row++ ) {
+				for( int col = 0; col < matrix.getColumns(); col++ ) {
+					if( skipZeros && matrix.getSimilarity( row , col) == 0.0d ) continue;
+					p.println( row + "," + col + "," + Utility.roundDouble( matrix.getSimilarity( row , col), 4) );
+				}
+				if( doIsolines ) p.println(""); // blank lines between scans
+			}
+			
+			p.close();
+			out.close();
+		}
+		
+		/*
+		AlignmentOutput output = new AlignmentOutput(matcher.getAlignmentSet(), fullFileName);
+		String sourceUri = Core.getInstance().getSourceOntology().getURI();
+		String targetUri = Core.getInstance().getTargetOntology().getURI();
+		output.write(sourceUri, targetUri, sourceUri, targetUri);
+		*/
 	}
 
 }
