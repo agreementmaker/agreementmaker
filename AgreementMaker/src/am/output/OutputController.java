e86a09235934978ffab9b76f7cc8bfef37e02b4b
Cosmin Stroe <cstroe@gmail.com>
2010-10-14 01:04:12 +0000

    Updated this class to use an enum.

--- AgreementMaker/src/am/output/OutputController.java
+++ AgreementMaker/src/am/output/OutputController.java
@@ -7,22 +7,46 @@ import java.util.Date;
 
 import am.app.Core;
 import am.app.mappingEngine.AbstractMatcher;
-
+/**
+ * Q: What do the methods of this class do? - Cosmin. (Oct 13th, 2010)
+ * A: They are responsible for saving the AlignmentSets in different formats. - Cosmin
+ */
 public class OutputController {
 
-	public final static String TXT = "txt";
-	public final static String DOC = "doc";
-	public final static String XLS = "xls";
-	public final static String RDF = "rdf";
+	
+	public static enum AlignmentFormats {
+		RDF("OAEI ( .rdf )", "rdf"),
+		TXT("Text ( .txt )", "txt"),
+		DOC("Word ( .doc )", "doc"),
+		XLS("Excel ( .xls )", "xls");
+		
+		String description, file_extension;
+		AlignmentFormats(String desc, String ext) { description = desc; file_extension = ext; }
+		String getDescription() { return description; }
+		String getFileExtension() { return file_extension; }
+	}
+	public static String[] getAlignmentFormatDescriptionList() {
+		AlignmentFormats[] array = AlignmentFormats.values();
+		String[] formatList = new String[ array.length ];
+		for( int i = 0; i < formatList.length; i++ ) {
+			formatList[i] = array[i].getDescription();
+		}
+		return formatList;
+	}
+	public static String getAlignmentFormatExtension( int index ) {
+		AlignmentFormats[] array = AlignmentFormats.values();
+		return array[index].getFileExtension();
+	}
 
 	public final static String arrow = "--->";
 	
 	public static void printDocumentOAEI(String name) throws Exception{
 		ArrayList<AbstractMatcher> list = Core.getInstance().getMatcherInstances();
 		AbstractMatcher matcher;
+		Core.getInstance();
 		//TO DO:
 		//May be multiple matchers.
-		int [] rowsIndex = Core.getInstance().getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
+		int [] rowsIndex = Core.getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
 		matcher = list.get(rowsIndex[0]);
 		AlignmentOutput output = new AlignmentOutput(matcher.getAlignmentSet(), name);
 		String sourceUri = Core.getInstance().getSourceOntology().getURI();
@@ -30,9 +54,11 @@ public class OutputController {
 		output.write(sourceUri, targetUri, sourceUri, targetUri);
 	}
 	
+	// TODO: This function should differentiate between TXT, DOC, and XLS formats. (Or create separate print functions for each format)
 	public static void printDocument(String name) throws Exception{
 		
-		int [] rowsIndex = Core.getInstance().getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
+		Core.getInstance();
+		int [] rowsIndex = Core.getUI().getControlPanel().getTablePanel().getTable().getSelectedRows();
 		
 		Date d = new Date();
 		String toBePrinted = "AGREEMENT DOCUMENT\n\n";
