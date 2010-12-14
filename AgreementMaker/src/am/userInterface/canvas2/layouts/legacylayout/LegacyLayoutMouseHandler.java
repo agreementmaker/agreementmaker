fecf733bb20ac126f8cb5105140cf5eeabaec09c
Cosmin Stroe <cstroe@gmail.com>
2010-12-14 05:49:47 +0000

    *** empty log message ***

--- AgreementMaker/src/am/userInterface/canvas2/layouts/legacylayout/LegacyLayoutMouseHandler.java
+++ AgreementMaker/src/am/userInterface/canvas2/layouts/legacylayout/LegacyLayoutMouseHandler.java
@@ -25,7 +25,6 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import am.Utility;
 import am.app.Core;
 import am.app.mappingEngine.AbstractMatcher.alignType;
 import am.app.ontology.Node;
