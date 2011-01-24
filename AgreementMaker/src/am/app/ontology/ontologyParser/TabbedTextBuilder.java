736fe7e04c313e56a1813f28a12cc19bab28ad31
Cosmin Stroe <cstroe@gmail.com>
2011-01-24 03:29:28 +0000

    Starting point.

---
+++ AgreementMaker/src/am/app/ontology/ontologyParser/TabbedTextBuilder.java
@@ -0,0 +1,16 @@
+package am.app.ontology.ontologyParser;
+
+public class TabbedTextBuilder extends TreeBuilder {
+
+	public TabbedTextBuilder(String filename, int sourceOrTarget,
+			String language, String format) {
+		super(filename, sourceOrTarget, language, format);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void buildTree() {
+		
+	}
+	
+}
