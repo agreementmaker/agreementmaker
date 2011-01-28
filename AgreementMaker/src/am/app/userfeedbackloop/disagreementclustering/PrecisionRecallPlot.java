81394fbb4cf86e271938666596b189d4d6d291e0
Cosmin Stroe <cstroe@gmail.com>
2011-01-28 00:38:58 +0000

    Joe's Commit.

---
+++ AgreementMaker/src/am/app/userfeedbackloop/disagreementclustering/PrecisionRecallPlot.java
@@ -0,0 +1,20 @@
+package am.app.userfeedbackloop.disagreementclustering;
+
+import am.app.mappingEngine.Alignment;
+import am.app.mappingEngine.Mapping;
+import am.app.userfeedbackloop.CandidateSelectionEvaluation;
+
+public class PrecisionRecallPlot extends CandidateSelectionEvaluation {
+
+	public PrecisionRecallPlot(Alignment<Mapping> rL, Alignment<Mapping> ref) {
+		super(rL, ref);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void evaluate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
