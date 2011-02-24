ef332fe198e52987c8c5a46e4acbc432cacd3778
Cosmin Stroe <cstroe@gmail.com>
2011-02-24 03:00:32 +0000

    Reworked Lexicon Store.

--- AgreementMaker/src/am/app/mappingEngine/LexiconStore.java
+++ AgreementMaker/src/am/app/mappingEngine/LexiconStore.java
@@ -1,21 +1,21 @@
 package am.app.mappingEngine;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
-import org.apache.log4j.Logger;
-
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.rdf.model.Property;
 
 import am.app.Core;
 import am.app.lexicon.Lexicon;
 import am.app.lexicon.LexiconBuilder;
+import am.app.lexicon.LexiconBuilderParameters;
 import am.app.lexicon.ontology.OntologyLexiconBuilder;
 import am.app.lexicon.wordnet.WordNetLexiconBuilder;
 import am.app.ontology.Ontology;
+import am.app.ontology.OntologyChangeEvent;
+import am.app.ontology.OntologyChangeListener;
+import am.app.ontology.OntologyChangeEvent.EventType;
 
-public class LexiconStore {
+public class LexiconStore implements OntologyChangeListener {
 	
 	/**
 	 * Lexicon Registry
@@ -23,15 +23,17 @@ public class LexiconStore {
 	 *
 	 */
 	public static enum LexiconRegistry {
-		ONTOLOGY_LEXICON		("Ontology Lexicon", OntologyLexiconBuilder.class),
-		WORDNET_LEXICON			("WordNet Lexicon" , WordNetLexiconBuilder.class);
+		ONTOLOGY_LEXICON		("Ontology Lexicon", "ONT", OntologyLexiconBuilder.class),
+		WORDNET_LEXICON			("WordNet Lexicon" , "WNet", WordNetLexiconBuilder.class);
 		
 		/* Don't change anything below this line .. unless you intend to. */
 		private String name;
+		private String shortName;
 		private Class<? extends LexiconBuilder> lexBuilder;
 		
-		LexiconRegistry( String n, Class<? extends LexiconBuilder> b ) { name = n; lexBuilder = b;}
+		LexiconRegistry( String n, String sn, Class<? extends LexiconBuilder> b ) { name = n; shortName = sn; lexBuilder = b;}
 		public String getLexiconName() { return name; }
+		public String getShortName() { return shortName; }
 		public Class<? extends LexiconBuilder> getLexiconBuilderClass() { return lexBuilder; }
 		public String toString() { return name; }
 	}
@@ -40,129 +42,125 @@ public class LexiconStore {
 	 * Lexicon Store
 	 */
 	
+	public LexiconStore() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	// Logger
-	private static Logger log = Logger.getLogger(LexiconStore.class);
+//	private static Logger log = Logger.getLogger(LexiconStore.class);
 	
 	List<Lexicon> lexList = new ArrayList<Lexicon>();
+
+	private LexiconBuilderParameters params;
 	
-	public void clear() { }
+	public void clear() {
+		params = null;
+		lexList = new ArrayList<Lexicon>();
+	}
 	
-	public void build(LexiconRegistry whichOne) {
+	public Lexicon build(LexiconRegistry whichOne, Ontology ont) throws Exception {
+
+		if( params == null )
+			throw new Exception("No parameters have been set for the lexicon builder.\nPlease use the Lexicon Builder Dialog to configure the parameters.\n\nThe Lexicon Builder Dialog can be accessed from the \"Lexicons -> Build all ...\" menu entry.");
 		
 		switch( whichOne ) {
 		case ONTOLOGY_LEXICON:
-			/*Ontology source;
-			Ontology target;
-			if( Core.getInstance().getSourceOntology() != null ) source = Core.getInstance().getSourceOntology();
-			if( Core.getInstance().getTargetOntology() != null ) target = Core.getInstance().getTargetOntology();*/
-			//OntologyLexiconBuilder ontLexBuilder = new OntologyLexiconBuilder(source, label, synonym, definition)
+		{
+			if( Core.getInstance().getSourceOntology() == ont ) {  // source
+				OntologyLexiconBuilder sourceOLB = new OntologyLexiconBuilder(ont, params.sourceUseLocalname, 
+						params.sourceSynonyms, params.sourceSynonyms, params.sourceDefinitions);
+	
+				Lexicon sourceOntologyLexicon = sourceOLB.buildLexicon();
+				sourceOntologyLexicon.setOntologyID(ont.getID());
+	
+				Core.getLexiconStore().registerLexicon(sourceOntologyLexicon); // register for reuse by other matchers
+				return sourceOntologyLexicon;
+			} else {  // target
+				OntologyLexiconBuilder targetOLB = new OntologyLexiconBuilder(ont, params.targetUseLocalname, 
+						params.targetSynonyms, params.targetSynonyms, params.targetDefinitions);
+	
+				Lexicon targetOntologyLexicon = targetOLB.buildLexicon();
+				targetOntologyLexicon.setOntologyID(ont.getID());
+	
+				Core.getLexiconStore().registerLexicon(targetOntologyLexicon); // register for reuse by other matchers
+				return targetOntologyLexicon;
+			}
+		}	
+		case WORDNET_LEXICON:
+		{
+			WordNetLexiconBuilder sourceOLB = new WordNetLexiconBuilder(ont, getLexicon(ont.getID(), LexiconRegistry.ONTOLOGY_LEXICON));
+
+			Lexicon sourceWordNetLexicon = sourceOLB.buildLexicon();
+			sourceWordNetLexicon.setOntologyID(ont.getID());
+
+			Core.getLexiconStore().registerLexicon(sourceWordNetLexicon);
+			return sourceWordNetLexicon;
+		}
+		default:
+			throw new Exception("Invalid lexicon type.");
 		}
 		
 	}
-	public void buildAll() { }
+	public void buildAll() throws Exception {
+		if( !Core.getInstance().ontologiesLoaded() ) {
+			throw new RuntimeException("You must load the source and target ontologies before you can build lexicons.");
+		}
+		
+		build(LexiconRegistry.ONTOLOGY_LEXICON, Core.getInstance().getSourceOntology() );
+		build(LexiconRegistry.ONTOLOGY_LEXICON, Core.getInstance().getTargetOntology() );
+		
+		build(LexiconRegistry.WORDNET_LEXICON, Core.getInstance().getSourceOntology() );
+		build(LexiconRegistry.WORDNET_LEXICON, Core.getInstance().getTargetOntology() );
+	}
 		
 
-	public Lexicon getLexicon( LexiconRegistry whichOne ) {
+	public Lexicon getLexicon( int ontologyID, LexiconRegistry type ) throws Exception {
 		for( Lexicon lex : lexList ) {
-			if( lex.getRegistryEntry() == whichOne ) return lex;
+			if( lex.getOntologyID() == ontologyID && lex.getType() == type ) return lex;
 		}
-		return null;
+		// lexicon was not found.
+		return build(type, Core.getInstance().getOntologyByID(ontologyID));
 	}
 	
-	public List<Lexicon> getLexiconsByOntID( int id ) {
+	public List<Lexicon> getLexicons( int ontologyID ) {
 		ArrayList<Lexicon> ret = new ArrayList<Lexicon>();
 		for( Lexicon lex : lexList ) {
-			if( lex.getOntologyID() == id ) ret.add(lex);
+			if( lex.getOntologyID() == ontologyID ) ret.add(lex);
 		}
 		return ret;
 	}
 
-	public Lexicon getLexiconByOntIDAndType( int id , LexiconRegistry type ) {
-		for( Lexicon lex : lexList ) {
-			if( lex.getOntologyID() == id && lex.getRegistryEntry() == type) return lex;
-		}
-		return null;
-	}
-	
+
 	public void registerLexicon(Lexicon sourceLexicon) {
+		
+		// check to make sure that we dont have the same type of lexicon for an ontology
+		for( Lexicon l : lexList ) {
+			if( l.getOntologyID() == sourceLexicon.getOntologyID() && 
+					l.getType() == sourceLexicon.getType() ) {
+				lexList.remove(l);
+				break;
+			}
+		}
+		
 		lexList.add(sourceLexicon);
 		
 	}
 
-	public Lexicon getSourceOntLexicon(Ontology sourceOntology) {
-		Lexicon sourceOntologyLexicon = Core.getLexiconStore().getLexiconByOntIDAndType( sourceOntology.getID(), LexiconRegistry.ONTOLOGY_LEXICON );
-		
-		if( sourceOntologyLexicon == null ) {
-			log.info("Building source ontology lexicon...");
-			OntModel sourceModel = sourceOntology.getModel();
-			
-			// the synonym property, label property and definition property
-			Property sourceSynonymProperty = sourceModel.getProperty("http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym");
-			Property sourceLabelProperty = sourceModel.getProperty("http://www.w3.org/2000/01/rdf-schema#label");
-			Property sourceDefinitionProperty = sourceModel.getProperty("http://www.geneontology.org/formats/oboInOwl#hasDefinition");
-			
-			OntologyLexiconBuilder sourceOLB = new OntologyLexiconBuilder(sourceOntology, sourceLabelProperty, sourceSynonymProperty, sourceDefinitionProperty);
-			
-			sourceOntologyLexicon = sourceOLB.buildLexicon();
-			sourceOntologyLexicon.settOntologyID(sourceOntology.getID());
-			
-			Core.getLexiconStore().registerLexicon(sourceOntologyLexicon); // register for reuse by other matchers
+	@Override
+	public void ontologyChanged(OntologyChangeEvent e) {
+		if( e.getEvent() == EventType.ONTOLOGY_REMOVED ) {
+			Iterator<Lexicon> lexIter = lexList.iterator();
+			while( lexIter.hasNext() ) {
+				Lexicon currentLexicon = lexIter.next();
+				if( currentLexicon.getOntologyID() == e.getOntologyID() ) {
+					// this ontology was removed, remove its lexicon.
+					lexIter.remove();
+				}
+			}
 		}
-		
-		return sourceOntologyLexicon;
-		
 	}
-	
-	public Lexicon getTargetOntLexicon(Ontology targetOntology ) {
-		Lexicon targetOntologyLexicon = Core.getLexiconStore().getLexiconByOntIDAndType( targetOntology.getID(), LexiconRegistry.ONTOLOGY_LEXICON);
-		
-		if( targetOntologyLexicon == null ) {
-			log.info("Building target ontology lexicon...");
-			OntModel targetModel = targetOntology.getModel();
-	
-			// the synonym property, label property and definition property
-			Property targetSynonymProperty = targetModel.getProperty("http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym");	
-			Property targetLabelProperty = targetModel.getProperty("http://www.w3.org/2000/01/rdf-schema#label");
-			Property targetDefinitionProperty = targetModel.getProperty("http://www.geneontology.org/formats/oboInOwl#hasDefinition");
-			
-			// STEP 1: Let's build the lexicons for the ontology
-			OntologyLexiconBuilder targetOLB = new OntologyLexiconBuilder(targetOntology, targetLabelProperty, targetSynonymProperty, targetDefinitionProperty);
 
-			targetOntologyLexicon = targetOLB.buildLexicon();
-			targetOntologyLexicon.settOntologyID(targetOntology.getID());
-			
-			Core.getLexiconStore().registerLexicon(targetOntologyLexicon);
-		}
-		return targetOntologyLexicon;
-	}
-	
-	public Lexicon getSourceWNLexicon(Ontology sourceOntology, Lexicon sourceOntologyLexicon) {
-		Lexicon sourceWordNetLexicon = Core.getLexiconStore().getLexiconByOntIDAndType( sourceOntology.getID(), LexiconRegistry.WORDNET_LEXICON);
-		
-		if( sourceWordNetLexicon == null ) { // no lexicon available we must build it
-			log.info("Building source WordNet lexicon...");
-			// STEP 1: Let's build the lexicons for the ontology
-			WordNetLexiconBuilder sourceOLB = new WordNetLexiconBuilder(sourceOntology, sourceOntologyLexicon);
-
-			sourceWordNetLexicon = sourceOLB.buildLexicon();
-			sourceWordNetLexicon.settOntologyID(sourceOntology.getID());
-			
-			Core.getLexiconStore().registerLexicon(sourceWordNetLexicon);
-		}
-		return sourceWordNetLexicon;
-	}
-	
-	public Lexicon getTargetWNLexicon(Ontology targetOntology, Lexicon targetOntologyLexicon) {
-		Lexicon targetWordNetLexicon = Core.getLexiconStore().getLexiconByOntIDAndType( targetOntology.getID(), LexiconRegistry.WORDNET_LEXICON);
-		if( targetWordNetLexicon == null ) {
-			log.info("Building target WordNet lexicon...");
-			WordNetLexiconBuilder targetOLB = new WordNetLexiconBuilder(targetOntology, targetOntologyLexicon);
-			
-			targetWordNetLexicon = targetOLB.buildLexicon();
-			targetWordNetLexicon.settOntologyID( targetOntology.getID() );
-			
-			Core.getLexiconStore().registerLexicon(targetWordNetLexicon);
-		}
-		return targetOntologyLexicon;
+	public void setParameters(LexiconBuilderParameters params) {
+		this.params = params;	
 	}
 }
