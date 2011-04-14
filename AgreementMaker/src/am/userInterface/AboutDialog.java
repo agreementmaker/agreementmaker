cb12f2bdc74a38b8c63ef85b7354a3a798e85c36
Cosmin Stroe <cstroe@gmail.com>
2011-04-14 22:17:21 +0000

    Updated version info.

--- AgreementMaker/src/am/userInterface/AboutDialog.java
+++ AgreementMaker/src/am/userInterface/AboutDialog.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -13,22 +14,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
 
 import am.GlobalStaticVariables;
 import am.app.Core;
 
-public class AboutDialog implements ActionListener {
+public class AboutDialog extends JDialog implements ActionListener {
 		
-	
-  private final static byte am[]={ // don't touch
+	private static final long serialVersionUID = -1107442375488915232L;
+
+	private final static byte am[]={ // don't touch
 	    -119,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,61,0,0,0,21,1,3,0,0,0,-49,83,116,-15,0,0,0,1,115,82,
 	    71,66,0,-82,-50,28,-23,0,0,0,6,80,76,84,69,-1,-1,-1,0,0,0,85,-62,-45,126,0,0,0,1,98,75,71,68,0,-120,5,29,72,0,0,
 	    0,9,112,72,89,115,0,0,46,35,0,0,46,35,1,120,-91,63,118,0,0,0,7,116,73,77,69,7,-40,11,26,9,45,18,-90,-113,-94,32,0,0,
@@ -38,7 +46,7 @@ public class AboutDialog implements ActionListener {
 	    25,12,42,112,48,-6,97,-116,54,48,3,0,76,57,24,0,37,7,126,100,0,0,0,0,73,69,78,68,-82,66,96,-126
 		  };
 	
-	private JDialog frameAbout;
+	//private JDialog frameAbout;
 	private JButton close, cartoon;
 	private JLabel image_label;
 	private ImageIcon label_icon;
@@ -52,14 +60,12 @@ public class AboutDialog implements ActionListener {
 	/**
 	 * This is the part of the program where the REAL fun stuff happens.
 	 */
-	public AboutDialog() {
-		
+	public AboutDialog(Frame parent) {
+		super(parent, true);
 		/** Main window */
-		frameAbout = new JDialog(Core.getUI().getUIFrame(), true);
-		frameAbout.setTitle("About AgreementMaker...");
-		frameAbout.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		frameAbout.setResizable(false);
-		
+		setTitle("About AgreementMaker...");
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setResizable(false);
 		
 		
 		/***************** TITLE ******************/
@@ -84,42 +90,54 @@ public class AboutDialog implements ActionListener {
 		JPanel credits = new JPanel();
 		credits.setLayout(new BoxLayout(credits, BoxLayout.Y_AXIS));
 		
-		ArrayList<JLabel> l = new ArrayList<JLabel>();
-		l.add( new JLabel("Professor Isabel Cruz") );					// 0
-		
-		//l.add( new JLabel("Advances in Information Systems Laboratory") ); 
+		ArrayList<Component> l = new ArrayList<Component>();
+				//l.add( new JLabel("Advances in Information Systems Laboratory") ); 
 		//l.add( new JLabel("University of Illinois at Chicago") );          
 
+		l.add( new JLabel("Professor Isabel Cruz") );					// 0
+		l.add( Box.createVerticalStrut(10) );
+		l.add( new JLabel("AgreementMaker v0.23 ( April 14th 2011 )"));   // 1
+		l.add( new JLabel("Joe Lozar"));   // 1
+		l.add( Box.createVerticalStrut(10) );
 		l.add( new JLabel("AgreementMaker v0.22 ( Dec 25th 2010 )"));   // 1
 		l.add( new JLabel("Michele Caci, Matteo Palmonari, Federico Caimi")); // 2
+		l.add( Box.createVerticalStrut(10) );
 		l.add( new JLabel("AgreementMaker v0.21 ( Aug 19th 2010 )"));  // 3
+		l.add( Box.createVerticalStrut(10) );
 		l.add( new JLabel("AgreementMaker v0.2 ( fall 2008 - 2009):") );  // 4
 		l.add( new JLabel("Flavio Palandri Antonelli, Cosmin Stroe, Ula\u0219 Kele\u0219.") );  // Ulas Keles  // 5
+		l.add( Box.createVerticalStrut(10) );
 		l.add( new JLabel("AgreementMaker v0.1 (2001 - 2008):") );   // 6
 		l.add( new JLabel("Afsheen Rajendran, Anjli Chaudhry, Nalin Makar,") );  // 7
 		l.add( new JLabel("Sarang Kapadia, Sujan Bathala, William Sunna.") ); //8
 		
-		for(int i=0; i<3; i++ ) { l.get(i).setAlignmentX(Component.CENTER_ALIGNMENT); }
-		for(int i=3; i<l.size(); i++ ) { l.get(i).setAlignmentX(Component.CENTER_ALIGNMENT); }
+		for(int i=0; i< l.size(); i++ ) { 
+			if( l.get(i) instanceof JLabel ) {
+				((JLabel)l.get(i)).setAlignmentX(Component.CENTER_ALIGNMENT);
+			}
+		}
 
-		l.get(0).setFont(new Font("Helvetica", Font.PLAIN,  22));
-		l.get(1).setFont(new Font("Helvetica", Font.BOLD,  14));
-		l.get(2).setFont(new Font("Helvetica", Font.PLAIN,  14));
-		l.get(3).setFont(new Font("Helvetica", Font.BOLD,   14));
-		l.get(4).setFont(new Font("Helvetica", Font.BOLD,   14));
-		l.get(5).setFont(new Font("Helvetica", Font.PLAIN,   14));
-		l.get(6).setFont(new Font("Helvetica", Font.BOLD,  14));
-		l.get(7).setFont(new Font("Helvetica", Font.PLAIN,   14));
-		l.get(8).setFont(new Font("Helvetica", Font.PLAIN,  14));
+		int index = 0;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,  22)); index++;
+		index++;
+		l.get(index).setFont(new Font("Helvetica", Font.BOLD,  14)); index++;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,  14)); index++;
+		index++;
+		l.get(index).setFont(new Font("Helvetica", Font.BOLD,  14)); index++;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,  14)); index++;
+		index++;
+		l.get(index).setFont(new Font("Helvetica", Font.BOLD,   14)); index++;
+		index++;
+		l.get(index).setFont(new Font("Helvetica", Font.BOLD,   14)); index++;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,   14)); index++;
+		index++;
+		l.get(index).setFont(new Font("Helvetica", Font.BOLD,  14)); index++;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,   14)); index++;
+		l.get(index).setFont(new Font("Helvetica", Font.PLAIN,  14)); index++;
 		
-		//credits.add(l.get(0));
 		credits.add(Box.createVerticalStrut(10));
 
-		for( int i=1; i<l.size(); i++) {
-			credits.add(l.get(i));
-			//if( i == 1 || i == 4 || i == 6 ) credits.add(Box.createVerticalStrut(5));
-			if( i == 2 || i == 3 || i == 5) credits.add(Box.createVerticalStrut(10));
-		}
+		for( int i=0; i<l.size(); i++) { credits.add(l.get(i)); }
 
 		credits.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		
@@ -151,31 +169,49 @@ public class AboutDialog implements ActionListener {
 		bottom.add(cartoon);
 		
 		BorderLayout layout = new BorderLayout(5,5);
-		frameAbout.setLayout(layout);
-		frameAbout.setSize(620, 500);
+		setLayout(layout);
+		setSize(620, 500);
 		
 		
 
 		
 		//frameAbout.add(title, BorderLayout.NORTH);
-		frameAbout.add(middle, BorderLayout.CENTER);
+		add(middle, BorderLayout.CENTER);
 		
-		frameAbout.add(bottom, BorderLayout.SOUTH);
+		add(bottom, BorderLayout.SOUTH);
 		
-		frameAbout.pack();
-		frameAbout.setLocationRelativeTo(null);
-		frameAbout.setVisible(true);
-		
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
 		
 	}
 
+	// make the escape key work
+	@Override
+	protected JRootPane createRootPane() {
+		JRootPane rootPane = new JRootPane();
+		KeyStroke stroke = KeyStroke.getKeyStroke("ESCAPE");
+		Action actionListener = new AbstractAction() {
+			private static final long serialVersionUID = 1774539460694983567L;
+			public void actionPerformed(ActionEvent actionEvent) {
+				close.doClick();
+			}
+		};
+		InputMap inputMap = rootPane
+		.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		inputMap.put(stroke, "ESCAPE");
+		rootPane.getActionMap().put("ESCAPE", actionListener);
+
+		return rootPane;
+	}
+	
 	public void actionPerformed(ActionEvent arg0) {
 		
 		
 		Object o = arg0.getSource();
 		
 		if( o.equals(close) ) {
-			frameAbout.dispose();
+			setVisible(false);
 		} else if( o.equals(cartoon)) {
 
 			
