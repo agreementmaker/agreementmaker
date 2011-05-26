a250a420b71c13cfd6e683a2f0de04abd6f18d35
Cosmin Stroe <cstroe@gmail.com>
2011-05-26 20:00:47 +0000

    Added preamble.

--- AgreementMaker/src/am/app/mappingEngine/qualityEvaluation/metrics/GlobalConfidenceQuality.java
+++ AgreementMaker/src/am/app/mappingEngine/qualityEvaluation/metrics/GlobalConfidenceQuality.java
@@ -1,3 +1,22 @@
+/**    ________________________________________
+ * ___/ Copyright Notice / Warranty Disclaimer \________________
+ *
+ * @copyright { Copyright (c) 2010
+ * Advances in Information Systems Laboratory at the
+ * University of Illinois at Chicago
+ * All Rights Reserved. }
+ * 
+ * @disclaimer {
+ * This work is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. }
+ * 
+ *     _____________________
+ * ___/ Authors / Changelog \___________________________________          
+ * 
+ *  
+ */
+
 package am.app.mappingEngine.qualityEvaluation.metrics;
 
 import am.Utility;
