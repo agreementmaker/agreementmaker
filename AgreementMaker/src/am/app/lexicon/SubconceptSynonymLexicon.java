fa12e19d2079962e7727942e5ca1f98d4653cb8f
Cosmin Stroe <cstroe@gmail.com>
2011-07-20 19:58:29 +0000

    Subconcept Synonym additions to the Lexicon.

---
+++ AgreementMaker/src/am/app/lexicon/SubconceptSynonymLexicon.java
@@ -0,0 +1,15 @@
+package am.app.lexicon;
+
+/**
+ * This method implements a lexicon that finds word in the concept string that 
+ * are synonyms.
+ * 
+ * if "a b c" hasSynonym "d b c" then "a" is a synonym word of "d". 
+ * 
+ * @author Cosmin M. Stroe
+ */
+public interface SubconceptSynonymLexicon extends Lexicon {
+
+	
+	
+}
