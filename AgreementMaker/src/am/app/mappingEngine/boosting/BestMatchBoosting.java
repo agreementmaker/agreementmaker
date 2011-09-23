00763f067c014f3e0bca6a4de8b15c49813b5c1b
Cosmin Stroe <cstroe@gmail.com>
2011-09-23 03:31:08 +0000

    BestMatchBoosting changed to integrate better in AM.  Added deepCopy option, algorithm unchanged.

--- AgreementMaker/src/am/app/mappingEngine/boosting/BestMatchBoosting.java
+++ AgreementMaker/src/am/app/mappingEngine/boosting/BestMatchBoosting.java
@@ -1,79 +1,71 @@
 package am.app.mappingEngine.boosting;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import am.app.mappingEngine.AbstractMatcher;
 import am.app.mappingEngine.AbstractMatcherParametersPanel;
-import am.app.mappingEngine.Alignment;
 import am.app.mappingEngine.Mapping;
 import am.app.mappingEngine.SimilarityMatrix;
+import am.app.ontology.Node;
 
 /**
  * This matcher takes another matcher as input and increases the similarities between reciprocal best matches by a factor.
  * @author cpesquita
- *
+ * @date Sept 22, 2011
  */
-public class BestMatchBoosting extends AbstractMatcher{
+public class BestMatchBoosting extends AbstractMatcher {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -6613515980650628944L;
 
-	/**
-	 * 
-	 */
+	public BestMatchBoosting() { super(); }
 
-	private AbstractMatcher input;
-
-
-
-	public BestMatchBoosting() {
-		super();
-		needsParam = true;
-	}
-
+	@Override
 	protected void initializeVariables() {
 		super.initializeVariables();
 		minInputMatchers = 1;
-
+		maxInputMatchers = 1;
+		needsParam = true;
 	}
 
 	@Override
-	protected void beforeAlignOperations() throws Exception{
-		super.beforeAlignOperations();
-		if( inputMatchers.size() != 1 ) {
-			throw new RuntimeException("Best Match Boosting needs to have one input matcher.");
-		}
-
-		input = inputMatchers.get(0);
-
-		
-
-	}
-
-	@Override
-	public void afterAlignOperations() {
-		super.afterAlignOperations();
-	
-
-		// we must clone the AlignmentMatrix, otherwise we will be changing the alignment matrix of the input matcher.
-		classesMatrix =input.getClassesMatrix().clone();  // clone
+	protected SimilarityMatrix alignNodesOneByOne(List<Node> sourceList,
+			List<Node> targetList, alignType typeOfNodes) throws Exception {
 	
 		BestMatchBoostingParameters bp = (BestMatchBoostingParameters)param;
+
+		SimilarityMatrix matrix = null;
+		
+		// we must clone the AlignmentMatrix, otherwise we will be changing the alignment matrix of the input matcher.
+		if( bp.deepCopy ) { // clone
+			if( typeOfNodes == alignType.aligningClasses ) {
+				matrix = inputMatchers.get(0).getClassesMatrix().clone();
+			}
+			else { 
+				matrix = inputMatchers.get(0).getPropertiesMatrix().clone();
+			}
+		} else { // the user doesn't mind if we edit the matrix of the original matcher
+			if( typeOfNodes == alignType.aligningClasses ) {
+				matrix = inputMatchers.get(0).getClassesMatrix();
+			}
+			else { 
+				matrix = inputMatchers.get(0).getPropertiesMatrix();
+			}
+		}
+	
 		//double[][] similarityMatrix = matrix.getCopiedSimilarityMatrix(); //hungarian alg needs a double matrix
 
-		int rows = classesMatrix.getRows();
-		int cols = classesMatrix.getColumns();
+		int rows = matrix.getRows();
+		int cols = matrix.getColumns();
 		ArrayList<Mapping> bestmatchesRows = new ArrayList<Mapping>();
 		ArrayList<Mapping> bestmatchesCols = new ArrayList<Mapping>();
 
 		for(int i=0;i<rows;i++){
-			Mapping m=classesMatrix.getRowMaxValues(i, 1)[0];
+			Mapping m=matrix.getRowMaxValues(i, 1)[0];
 			bestmatchesRows.add(m);
 		}
 		for(int j=0;j<cols;j++){
-			Mapping m=classesMatrix.getColMaxValues(j, 1)[0];
+			Mapping m=matrix.getColMaxValues(j, 1)[0];
 			bestmatchesCols.add(m);
 		}
 
@@ -83,21 +75,27 @@ public class BestMatchBoosting extends AbstractMatcher{
 					if(bestmatchesRows.get(i).equals(bestmatchesCols.get(j))){
 
 
-						Mapping newMap = classesMatrix.get(i, j);
+						Mapping newMap = matrix.get(i, j);
 						double newSim=newMap.getSimilarity()*bp.boostPercent;
 						if(newSim>1.0)
 							newSim=1.0;
 						newMap.setSimilarity(newSim);
-						classesMatrix.set(i, j, newMap);
+						matrix.set(i, j, newMap);
 					}
 
+				}
+				
+				if( isProgressDisplayed() ) {
+					stepDone();
+					updateProgress();
 				}
 			}
 		}
 
-
+		return matrix;
 	}
 
+	@Override
 	public AbstractMatcherParametersPanel getParametersPanel() {
 		if(parametersPanel == null){
 			parametersPanel = new BestMatchBoostingParametersPanel();
--- AgreementMaker/src/am/app/mappingEngine/boosting/BestMatchBoostingParameters.java
+++ AgreementMaker/src/am/app/mappingEngine/boosting/BestMatchBoostingParameters.java
@@ -2,15 +2,19 @@ package am.app.mappingEngine.boosting;
 
 import am.app.mappingEngine.AbstractParameters;
 
+/**
+ * @author cpesquita
+ * @date Sept 22, 2011
+ */
 public class BestMatchBoostingParameters extends AbstractParameters{
 	
-	public double boostPercent;
+	private static final long serialVersionUID = 194591157273240997L;
+	
+	public double boostPercent = 1.1; // the multiplicative boost factor that similarities will be modified by.
+	public boolean deepCopy = false;  // deep copy of similarity matrix. if false, operate on the input matcher's similarity matrix.
 
-	public BestMatchBoostingParameters(double boostPercent) {
-		super();
-		this.boostPercent = boostPercent;
+	public BestMatchBoostingParameters() { super(); }
+	public BestMatchBoostingParameters(double threshold, int maxSourceAlign, int maxTargetAlign) {
+		super(threshold, maxSourceAlign, maxTargetAlign);
 	}
-	
-	
-
 }
