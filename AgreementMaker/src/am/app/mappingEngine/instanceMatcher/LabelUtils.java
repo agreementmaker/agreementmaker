c52a34e09c7fcd480728313c79d759306f1f6f5a
Federico Caimi <fedeoasi@gmail.com>
2011-11-05 01:10:11 +0000

    added person label processing

--- AgreementMaker/src/am/app/mappingEngine/instanceMatcher/LabelUtils.java
+++ AgreementMaker/src/am/app/mappingEngine/instanceMatcher/LabelUtils.java
@@ -48,6 +48,42 @@ public class LabelUtils {
 		return label;
 	}
 	
+
+	//TODO fix this method
+	public static String processPersonLabel(String label) {
+		if(label.contains("(")){
+			int beg = label.indexOf('(');
+			int end = label.indexOf(')');
+			label = label.substring(0,beg) + label.substring(end + 1);
+			label = label.trim();
+		}
+		
+		if(label.contains(",")){
+			String[] split = label.split(",");
+			label = split[1].trim() + " " + split[0].trim();
+		}
+				
+		String[] blackList = { "Jr" };
+		
+		String[] split = label.split(" ");
+		
+		label = "";		
+		for (int i = 0; i < split.length; i++) {
+			boolean toBlock = false;			
+			
+			for (int j = 0; j < blackList.length; j++) {
+				if(split[i].equalsIgnoreCase(blackList[j])){
+					toBlock = true;
+					break;
+				}
+			}
+			
+			if(!toBlock) label += split[i] + " ";
+		}
+		return label;
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println(LabelUtils.processOrganizationLabel("Omnicom Group Incorporated"));
 		System.out.println(LabelUtils.processOrganizationLabel("Ese Corporation"));
@@ -55,5 +91,9 @@ public class LabelUtils {
 		System.out.println(LabelUtils.processOrganizationLabel("Signature Bank"));
 		System.out.println(LabelUtils.processOrganizationLabel("Roundabout Theater Co"));
 		System.out.println(LabelUtils.processOrganizationLabel("Protective Life Corporation"));
+		
+		System.out.println(LabelUtils.processPersonLabel("Federico Jr Caimi"));
 	}
+	
+	
 }
