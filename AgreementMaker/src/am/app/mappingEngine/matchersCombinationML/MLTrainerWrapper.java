1619ad57075d271d730f2efa0c54ed315573538e
Vivek Hariharan <vharih3@uic.edu>
2011-11-12 21:55:39 +0000

    added more general methods

--- AgreementMaker/src/am/app/mappingEngine/matchersCombinationML/MLTrainerWrapper.java
+++ AgreementMaker/src/am/app/mappingEngine/matchersCombinationML/MLTrainerWrapper.java
@@ -70,41 +70,86 @@ public class MLTrainerWrapper {
 		}
 	}
 	
-	void callProcess()
+	void generateMappings()
 	{
-		String filename="bench/training.xml";
-		String elementname="trainingset";
-		loadMatchers();
-		loadOntologyTriples(filename,elementname);
-		
 		for(int t=0;t<listOfTriples.size();t++)
 		{
 			OntologyTriple currentTriple=listOfTriples.get(t);
-			System.out.println(currentTriple.getOntology1().getFilename());
-			System.out.println(currentTriple.getOntology1().getDescription());
-			System.out.println(currentTriple.getOntology1().getClassesList().size());
-			System.out.println(currentTriple.getOntology2().getFilename());
-			System.out.println(currentTriple.getOntology2().getDescription());
-			System.out.println(currentTriple.getOntology2().getClassesList().size());
-			
-			
-			
-			//for(int m=0;m<listOfMatchers.size();m++)
-			//{
-				//AbstractMatcher currentMatcher=listOfMatchers.get(m);
+			ArrayList<AbstractMatcher> matchers=currentTriple.getListOfMatchers();
+			for(int m=0;m<matchers.size();m++)
+			{
 				
-			//}
+				try {
+						AbstractMatcher currentMatcher=matchers.get(m);
+						currentMatcher.setOntologies(currentTriple.getOntology1(), currentTriple.getOntology2());
+						currentMatcher.match();
+						Alignment<Mapping> resultAlignment=currentMatcher.getAlignment();
+						if(resultAlignment!=null)
+						{
+							currentTriple.setAlignmentObtained(currentMatcher, resultAlignment);	
+						}
+						else
+						{
+							currentTriple.setAlignmentObtained(currentMatcher, null);
+						}
+					
+					
+					} catch (Exception e) {
+			
+						e.printStackTrace();
+					}
+			}
 		}
+	}
+	
+	void generateTrainingFile()
+	{
+		for(int t=0;t<listOfTriples.size();t++)
+		{
+			OntologyTriple currentTriple=listOfTriples.get(t);
+			ArrayList<AbstractMatcher> matchers=currentTriple.getListOfMatchers();
+			if(matchers!=null)
+			{
+				for(int m=0;m<matchers.size();m++)
+				{
+					AbstractMatcher currentMatcher=matchers.get(m);
+					Alignment<Mapping> currentMapping=currentTriple.getAlignmentObtained(currentMatcher);
+					if(currentMapping!=null)
+					{
+						
+					}
+				}	
+			}
+			
+			
+		}
+		
+	}
+	
+	void generateModel()
+	{
+		
+	}
+	
+	void callProcess()
+	{
+		String trainingFileName="bench/training.xml";
+		String elementName="trainingset";
+		loadMatchers();
+		loadOntologyTriples(trainingFileName,elementName);
+		generateMappings();
+		generateTrainingFile();
+		generateModel();
+//		String testFileName="";
+//		elementName="testset";
+//		loadOntologyTriples(testFileName,elementName);
 	}
 	
 	public static void main(String args[])
 	{
 		MLTrainerWrapper ml=new MLTrainerWrapper();
 		ml.callProcess();
-		
-		
-		
-		
+				
 	}
 
 }
