08744fb7437c58dc8ec7fd65b6ca64c43ab097f4
Josef Lozar <jlozar2@uic.edu>
2012-03-30 20:59:51 +0000

    initial commit

---
+++ AgreementMaker-Matchers/ConceptMatcher/src/conceptmatcher/bundle/Activator.java
@@ -0,0 +1,35 @@
+package conceptmatcher.bundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import conceptmatcher.internal.ConceptMatcher;
+
+import am.app.mappingEngine.AbstractMatcher;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		context.registerService(AbstractMatcher.class, new ConceptMatcher(), null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ AgreementMaker-Matchers/ConceptMatcher/src/conceptmatcher/internal/ConceptMatcherParameters.java
@@ -0,0 +1,11 @@
+package conceptmatcher.internal;
+
+import am.app.mappingEngine.DefaultMatcherParameters;
+
+public class ConceptMatcherParameters extends DefaultMatcherParameters {
+
+	public double DescendantSetWeight = 0.75d;
+	public double AncestorSetWeight = 0.75d;
+	public double TextSimilarityWeight = 0.75d;
+	
+}
