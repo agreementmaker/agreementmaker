42d0c7f4fb6485ab4743fe9559a412627989cf9b
Josef Lozar <jlozar2@uic.edu>
2012-03-30 20:01:20 +0000

    initial commit

---
+++ AgreementMaker-Matchers/IterativeMatcher/src/iterativematcher/bundle/Activator.java
@@ -0,0 +1,35 @@
+package iterativematcher.bundle;
+
+import iterativematcher.internal.IterativeMatcher;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import am.app.mappingEngine.AbstractMatcher;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		context.registerService(AbstractMatcher.class, new IterativeMatcher(), null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
