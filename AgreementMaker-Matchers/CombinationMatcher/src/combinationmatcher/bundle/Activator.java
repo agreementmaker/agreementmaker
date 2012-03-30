0af735414a8d42e384c25c88962d4f41a8eb6eae
Josef Lozar <jlozar2@uic.edu>
2012-03-30 20:50:29 +0000

    initial commit

---
+++ AgreementMaker-Matchers/CombinationMatcher/src/combinationmatcher/bundle/Activator.java
@@ -0,0 +1,35 @@
+package combinationmatcher.bundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import combinationmatcher.internal.CombinationMatcher;
+
+import am.app.mappingEngine.AbstractMatcher;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		context.registerService(AbstractMatcher.class, new CombinationMatcher(), null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
