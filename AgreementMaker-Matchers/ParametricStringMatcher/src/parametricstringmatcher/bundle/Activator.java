6005eddbc12f591ec224573999d2fa4414d8ec47
Josef Lozar <jlozar2@uic.edu>
2012-03-30 19:16:29 +0000

    inital commit

---
+++ AgreementMaker-Matchers/ParametricStringMatcher/src/parametricstringmatcher/bundle/Activator.java
@@ -0,0 +1,35 @@
+package parametricstringmatcher.bundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import parametricstringmatcher.external.ParametricStringMatcher;
+
+import am.app.mappingEngine.AbstractMatcher;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		context.registerService(AbstractMatcher.class, new ParametricStringMatcher(), null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
