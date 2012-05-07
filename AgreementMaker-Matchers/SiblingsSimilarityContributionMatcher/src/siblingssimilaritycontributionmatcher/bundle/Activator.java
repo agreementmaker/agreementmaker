73882707fdecda2f44b20768885220373f54cc87
Josef Lozar <jlozar2@uic.edu>
2012-05-07 22:00:25 +0000

    fixed some bugs

--- AgreementMaker-Matchers/SiblingsSimilarityContributionMatcher/src/siblingssimilaritycontributionmatcher/bundle/Activator.java
+++ AgreementMaker-Matchers/SiblingsSimilarityContributionMatcher/src/siblingssimilaritycontributionmatcher/bundle/Activator.java
@@ -21,7 +21,9 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		context.registerService(AbstractMatcher.class, new SiblingsSimilarityContributionMatcher(), null);
+		SiblingsSimilarityContributionMatcher matcher=new SiblingsSimilarityContributionMatcher();
+		matcher.setName("Sibling's Similarity Contribution");
+		context.registerService(AbstractMatcher.class.getName(), matcher, null);
 	}
 
 	/*
