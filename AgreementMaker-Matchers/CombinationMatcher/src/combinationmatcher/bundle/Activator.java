b7e9d644f0e2a1637572688421b55e71f5d78774
Josef Lozar <jlozar2@uic.edu>
2012-05-07 21:59:35 +0000

    fixed some bugs

--- AgreementMaker-Matchers/CombinationMatcher/src/combinationmatcher/bundle/Activator.java
+++ AgreementMaker-Matchers/CombinationMatcher/src/combinationmatcher/bundle/Activator.java
@@ -21,7 +21,9 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		context.registerService(AbstractMatcher.class, new CombinationMatcher(), null);
+		CombinationMatcher matcher=new CombinationMatcher();
+		matcher.setName("Linear Weighted Combination");
+		context.registerService(AbstractMatcher.class.getName(), matcher, null);
 	}
 
 	/*
