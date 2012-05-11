19b20f77373b6568ab2b8f2d541b18c582396e42
Josef Lozar <jlozar2@uic.edu>
2012-05-11 22:08:44 +0000

    started work on the ApproximativeSparseMatrix

--- AgreementMaker/src/am/app/mappingEngine/similarityMatrix/ApproximativeSparseMatrix.java
+++ AgreementMaker/src/am/app/mappingEngine/similarityMatrix/ApproximativeSparseMatrix.java
@@ -1,17 +1,60 @@
 package am.app.mappingEngine.similarityMatrix;
 
+import java.util.ArrayList;
+
 import am.app.mappingEngine.AbstractMatcher.alignType;
 import am.app.ontology.Ontology;
+import am.utility.Pair;
 
-public class ApproximativeSparseMatrix extends SparseMatrix {
+/**
+ * 
+ * @author joe
+ *
+ * This class extends the sparse matrix and does not include mappings that are less then the threshold that 
+ * is being used by the matcher.  By not including mappings that are less then the threshold, larger ontologies 
+ * are able to be matched because memory is saved.
+ *
+ */
 
-	private static final long serialVersionUID = -1578488061395125831L;
+public class ApproximativeSparseMatrix extends SparseMatrix{
+	
+	/** Holds sum and count of the mappings that are lower then the threshold
+	Each row and col will have its own index in the arraylist */ 
+	private ArrayList<Pair<Double, Integer>> rows, cols;
+	
+	/** the threshold of similarity that determines if the mapping is included in the matrix.
+	 * The .6D is the default threshold.  This can be changed by passing a threshold in the constructor or by using the setter method
+	 * getThreshold() */
+	private double threshold=.6D;
+	
 
-	public ApproximativeSparseMatrix(Ontology sourceOntology,
-			Ontology targetOntology, alignType typeOfMatrix, double threshold) {
+/**---------------------Constructors for the super class-------------------------------------------*/
+	public ApproximativeSparseMatrix(Ontology sourceOntology, Ontology targetOntology,
+			alignType typeOfMatrix) {
 		super(sourceOntology, targetOntology, typeOfMatrix);
-		// TODO Auto-generated constructor stub
 	}
-
-
+	
+	public ApproximativeSparseMatrix(SparseMatrix s){super(s);}
+/**------------------------------------------------------------------------------------------------*/
+	
+	
+/**---------------------Constructors that include the threshold as a param-------------------------*/	
+		public ApproximativeSparseMatrix(Ontology sourceOntology, Ontology targetOntology,
+				alignType typeOfMatrix, double threshold) {
+			super(sourceOntology, targetOntology, typeOfMatrix);
+			this.threshold=threshold;
+		}
+		
+		public ApproximativeSparseMatrix(SparseMatrix s, double threshold){
+			super(s);
+			this.threshold=threshold;
+		}
+/**------------------------------------------------------------------------------------------------*/
+	
+	
+	/** getters */
+	public double getThreshold() {return threshold;}
+	
+	/** setters */
+	public void setThreshold(double threshold) {this.threshold = threshold;}
 }
