6b3ab720275e9f5b92402510994c566a96280c42
Jeevan Joseph <jjosep37@uic.edu>
2012-11-01 18:37:35 +0000

    updating enum with comments

--- AgreementMaker/src/am/extension/semanticExplanation/RelationType.java
+++ AgreementMaker/src/am/extension/semanticExplanation/RelationType.java
@@ -7,3 +7,9 @@ public enum RelationType {
 	EXACTMATCH,
 	NOTDEFINED
 }
+//
+//SYNONYM("is a Synonym of", 1),
+//HYPERNYM("is a Hypernym of", 1),
+//HYPONYM("is a Jyponym of", 1),
+//EXACTMATCH("and a Synonym of", 1),
+//NOTDEFINED("is a Synonym of", 1)
