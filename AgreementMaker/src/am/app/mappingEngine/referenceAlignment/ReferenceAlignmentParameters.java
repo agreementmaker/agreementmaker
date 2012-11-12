278e507ea8937f386b79331546636aa5b612b263
Cosmin Stroe <cstroe@gmail.com>
2012-11-12 05:14:19 +0000

    Added some documentation.

--- AgreementMaker/src/am/app/mappingEngine/referenceAlignment/ReferenceAlignmentParameters.java
+++ AgreementMaker/src/am/app/mappingEngine/referenceAlignment/ReferenceAlignmentParameters.java
@@ -7,7 +7,20 @@ public class ReferenceAlignmentParameters extends DefaultMatcherParameters {
 	private static final long serialVersionUID = -565356899065786968L;
 	
 	public String fileName;
+	
+	/**
+	 * Format strings are static fields of ReferenceAlignmentMatcher.
+	 * 
+	 * @see {@link ReferenceAlignmentMatcher#OAEI},
+	 *      {@link ReferenceAlignmentMatcher#OLD_OAEI},
+	 *      {@link ReferenceAlignmentMatcher#REF2a},
+	 *      {@link ReferenceAlignmentMatcher#REF2b},
+	 *      {@link ReferenceAlignmentMatcher#REF2c},
+	 *      {@link ReferenceAlignmentMatcher#REF3},
+	 *      {@link ReferenceAlignmentMatcher#REF5}
+	 */
 	public String format;
+	
 	public boolean onlyEquivalence = false;
 	public boolean skipClasses = false;
 	public boolean skipProperties = false;
