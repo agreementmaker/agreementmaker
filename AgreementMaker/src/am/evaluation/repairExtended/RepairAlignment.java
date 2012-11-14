13998f91dc8c7a7357eaf1d5ccd685c26b73e12d
Pavan Reddy <preddy4@uic.edu>
2012-11-14 17:03:53 +0000

    Inconsistent equivalence axioms identified

--- AgreementMaker/src/am/evaluation/repairExtended/RepairAlignment.java
+++ AgreementMaker/src/am/evaluation/repairExtended/RepairAlignment.java
@@ -3,17 +3,30 @@ package am.evaluation.repairExtended;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.dom4j.DocumentException;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.HermiT.Reasoner.ReasonerFactory;
+import org.semanticweb.HermiT.structural.OWLAxioms;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.Node;
+
+import com.clarkparsia.owlapi.explanation.BlackBoxExplanation;
 
 import am.app.mappingEngine.referenceAlignment.MatchingPair;
 import am.app.mappingEngine.referenceAlignment.ReferenceAlignmentMatcher;
 import am.app.mappingEngine.referenceAlignment.ReferenceAlignmentParameters;
+import am.app.mappingEngine.referenceAlignment.ReferenceEvaluationData;
+import am.app.mappingEngine.referenceAlignment.ReferenceEvaluator;
 import am.evaluation.repair.AlignmentRepairUtilities;
 import am.utility.referenceAlignment.AlignmentUtilities;
 
@@ -21,12 +34,15 @@ public class RepairAlignment {
 
 	private static Logger log = Logger.getLogger(RepairAlignment.class);
 		
-	private File referenceFile = new File("/home/pavan/MS/WebSemantics/Project1/AgreementMaker/ontologies/Anatomy/reference.rdf");
-	private File sourceOwl = new File("/home/pavan/MS/WebSemantics/Project1/AgreementMaker/ontologies/Anatomy/human.owl");
-	private File targetOwl = new File("/home/pavan/MS/WebSemantics/Project1/AgreementMaker/ontologies/Anatomy/mouse.owl");
-	private File alignmentFile = new File("/home/pavan/MS/WebSemantics/Project1/AgreementMaker/ontologies/Anatomy/Alignment.rdf");
+	private File referenceFile = new File("/home/pavan/MS/WebSemantics/Ontologies/Anatomy/reference.rdf");
+	private File sourceOwl = new File("/home/pavan/MS/WebSemantics/Ontologies/Anatomy/mouse.owl");
+	private File targetOwl = new File("/home/pavan/MS/WebSemantics/Ontologies/Anatomy/human.owl");
+	private File alignmentFile = new File("/home/pavan/MS/WebSemantics/Ontologies/Anatomy/Alignment.rdf");
 	
 	private AlignmentRepairUtilities util = new AlignmentRepairUtilities(log);
+	
+	private List<MatchingPair> alignment;
+	private int axiomCount = 0;
 	
 	public static void main(String[] args) throws OWLOntologyCreationException {
 		
@@ -37,58 +53,127 @@ public class RepairAlignment {
 	public void repair(){
 		
 		OWLOntology mergedOntology;
+		List<OWLAxiom> inconsistentAxioms;
 		
 		try {
+						
+			computeMeasures(alignmentFile.toString(), referenceFile.toString());
+						
+			log.info("Merging ontolgoies...");
 			mergedOntology = mergeOntologies();
+			log.info("Ontologies merged successfully");
+						
+			log.info("Identifying inconsistent equivalence axioms...");
+			inconsistentAxioms = getInconsistentAxioms(alignment, mergedOntology);
+			log.info(axiomCount + " Inconsistent equivalence axioms identified");
 			
-			getInconsistentAxioms(mergedOntology);
+			
 			
 			
 		} catch (OWLOntologyCreationException e) {
-			log.info("OWL ontology merge failed");
+			log.error("OWL ontology merge failed");
 			e.printStackTrace();
 		} finally{
-			log.info("Repair Completed");
-		}
-		
+			log.info("Process complete");
+		}		
 	}
-	
-	
 	
 	public OWLOntology mergeOntologies()
 			throws OWLOntologyCreationException{
 		
-		log.info("Loading alignment...");
-		
-		List<MatchingPair> alignment = AlignmentUtilities.getMatchingPairsOAEI(alignmentFile.getAbsolutePath());
+		alignment = AlignmentUtilities.getMatchingPairsOAEI(alignmentFile.getAbsolutePath());
 		log.info("Loaded alignment. The alignment contains " + alignment.size() + " mappings.");
-		
-		log.info("Initial measures are:");
-		computeMeasures(alignmentFile.toString(), referenceFile.toString());
-		System.out.println(" ");
 		
 		return util.loadOntologies(alignment);
 	}
 	
-	public void getInconsistentAxioms(OWLOntology mergedOntology){
+	public List<OWLAxiom> getInconsistentAxioms(List<MatchingPair> alignment,OWLOntology mergedOntology){
 		
+		Node<OWLClass> unsatisfiledClasses;
+		List<OWLAxiom> inconsistentAxioms = new ArrayList<OWLAxiom>();
 		
+		ReasonerFactory reasonerFactory = new ReasonerFactory();
+		Reasoner reasoner = util.loadReasoner(mergedOntology);		
+				
+		unsatisfiledClasses = reasoner.getUnsatisfiableClasses();
+		
+		for(OWLClass cls : unsatisfiledClasses){		
+	
+			inconsistentAxioms.addAll((ArrayList<OWLAxiom>)getConflictingAxioms(mergedOntology, reasonerFactory, reasoner, cls));
+		}
+		
+		return inconsistentAxioms;		
+	}
+	
+	private List<OWLAxiom> getConflictingAxioms(OWLOntology mergedOntology,
+			ReasonerFactory reasonerFactory, Reasoner reasoner,
+			OWLClass unsatClass) {
+		//*** Get an explanation for each of the unsatisfiable classes.
+		//*** Save all the conflicting EquivalentClass axioms.
+		BlackBoxExplanation exp = new BlackBoxExplanation(mergedOntology, reasonerFactory, reasoner);
+		Set<OWLAxiom> expSet = exp.getExplanation(unsatClass);
+		List<OWLAxiom> conflictingAxioms = new ArrayList<OWLAxiom>();
+
+		//System.out.println("\n------------------------------ Unsatisfiable Class ------------------------------");
+		//log.info("The Unsatisfiable class is: " + unsatClass);
+
+		for(OWLAxiom causingAxiom : expSet) {
+//			log.info(causingAxiom.getAxiomType());
+			if(causingAxiom.getAxiomType() == AxiomType.EQUIVALENT_CLASSES) {
+				//Get the Source and Target classes in the axiom signature.
+				List<OWLClass> classList = new ArrayList<OWLClass>(causingAxiom.getClassesInSignature());
+				String[] sourceURI = classList.get(0).getIRI().toString().split("#");
+				String[] targetURI = classList.get(1).getIRI().toString().split("#");
+
+				//Save the conflicting axiom (if its not a mapping between classes in same ontology).
+				if(!sourceURI.equals(targetURI))
+					conflictingAxioms.add(causingAxiom);
+			}
+			else if(causingAxiom.getAxiomType() == AxiomType.EQUIVALENT_DATA_PROPERTIES) {
+				//Get the Source and Target classes in the axiom signature.
+				List<OWLClass> classList = new ArrayList<OWLClass>(causingAxiom.getClassesInSignature());
+				String[] sourceURI = classList.get(0).getIRI().toString().split("#");
+				String[] targetURI = classList.get(1).getIRI().toString().split("#");
+
+				//Save the conflicting axiom (if its not a mapping between classes in same ontology).
+				if(!sourceURI.equals(targetURI))
+					conflictingAxioms.add(causingAxiom);
+			}
+			else if(causingAxiom.getAxiomType() == AxiomType.EQUIVALENT_OBJECT_PROPERTIES) {
+				//Get the Source and Target classes in the axiom signature.
+				List<OWLClass> classList = new ArrayList<OWLClass>(causingAxiom.getClassesInSignature());
+				String[] sourceURI = classList.get(0).getIRI().toString().split("#");
+				String[] targetURI = classList.get(1).getIRI().toString().split("#");
+
+				//Save the conflicting axiom (if its not a mapping between classes in same ontology).
+				if(!sourceURI.equals(targetURI))
+					conflictingAxioms.add(causingAxiom);
+			}
+		}
+				
+		for(OWLAxiom confAx : conflictingAxioms){
+			//log.info("The conflicting axiom: " + confAx);
+			axiomCount++;
+		}
+						
+		return conflictingAxioms;
 	}
 	
 	public Double computeMeasures(String toEvaluate, String reference){
+		
 		int count = 0;
-
-		ReferenceAlignmentMatcher matcher = new ReferenceAlignmentMatcher();
-		ReferenceAlignmentParameters param = new ReferenceAlignmentParameters();
-
-		param.fileName = toEvaluate;
-		matcher.setParam(param);
-
 		ArrayList<MatchingPair> filePairs = null;
 		ArrayList<MatchingPair> refPairs = null;
-
+		
+		ReferenceAlignmentMatcher matcher = new ReferenceAlignmentMatcher();
+		ReferenceAlignmentParameters param = new ReferenceAlignmentParameters();
+		//matcher.setParam(param);
+		matcher.setParameters(param);
+		
 		try {
+			param.fileName = toEvaluate;
 			filePairs = matcher.parseStandardOAEI();
+			
 			param.fileName = reference.toString();
 			refPairs = matcher.parseStandardOAEI();
 
@@ -98,18 +183,23 @@ public class RepairAlignment {
 			e.printStackTrace();
 		}
 
+		int totalcount = 0;
+		
 		for (MatchingPair p1 : filePairs) {
 			for (MatchingPair p2 : refPairs) {
+				totalcount++;
 				if(p1.sourceURI.equals(p2.sourceURI) && p1.targetURI.equals(p2.targetURI)
 						&& p1.relation.equals(p2.relation)){
 					count++;
 					break;
-				}
+				}				
 			}
 		}
 		double precision = (double)count/filePairs.size();
 		double recall = (float)count/refPairs.size();
 		double fmeasure = 2 * precision * recall / (precision + recall);
+		
+		log.info("Initial measures...");
 		log.info("Precision: " + precision + " Recall: " + recall + " FMeasure: " + fmeasure);
 		System.out.println(" ");
 
