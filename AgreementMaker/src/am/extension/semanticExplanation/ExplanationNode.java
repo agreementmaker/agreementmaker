df75595c0d4ca9de22c3a01f7abaa37dd793dd1e
Jeevan Joseph <jjosep37@uic.edu>
2012-12-06 04:30:54 +0000

    1. Moved coded from ExplanationNode to SEmanticExpln 2. Moved explnMatrix to semanticExpln

--- AgreementMaker/src/am/extension/semanticExplanation/ExplanationNode.java
+++ AgreementMaker/src/am/extension/semanticExplanation/ExplanationNode.java
@@ -161,35 +161,7 @@ public class ExplanationNode {
 		return mspList;
 		
 	}
-	
-	/**
-	 *  Finds the Most significant path for the whole ontology. 
-	 * @param nodeMatrix
-	 * @param alignmentMappings
-	 * @return
-	 */
-	public static List<ExplanationNode> findUniversalMostSignificantPath(ExplanationNode[][] nodeMatrix, Alignment<Mapping> alignmentMappings){
-		Map<ExplanationNode,Integer> trafficMap = new HashMap<ExplanationNode, Integer>();
-		for(Mapping m:alignmentMappings){
-			ExplanationNode explanationNode = nodeMatrix[m.getEntity1().getIndex()][m.getEntity2().getIndex()];
-			List<ExplanationNode> significantPathForNode = findMostSignificantPath(explanationNode);
-			for(ExplanationNode node:significantPathForNode){
-				if(!trafficMap.containsKey(node)){
-					trafficMap.put(node, 1);
-				}
-				else{
-					int newValue = trafficMap.get(node) + 1;
-					trafficMap.put(node,newValue);
-				}
-			}
-		}
-		System.out.println("The final significantPathValues are:");
-		for(ExplanationNode key: trafficMap.keySet()){
-			System.out.println("Key= "+key.description+" value="+trafficMap.get(key));
-		}
-		return null;
-	}
-	
+
 	/**
 	 * @param ExplanationNode node
 	 * Finds the least significant path in an aligned mapping
--- AgreementMaker/src/am/extension/semanticExplanation/SemanticExpln.java
+++ AgreementMaker/src/am/extension/semanticExplanation/SemanticExpln.java
@@ -1,6 +1,63 @@
 package am.extension.semanticExplanation;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import am.app.mappingEngine.Alignment;
+import am.app.mappingEngine.Mapping;
+
 public class SemanticExpln {
-	public static ExplanationNode[][] explanationMatrix;
+	
+
+	private ExplanationNode[][] explanationMatrix;
+	
+	
+	
+	public SemanticExpln(int row, int col) {
+		super();
+		this.explanationMatrix = new ExplanationNode[row][col];
+	}
+
+
+	/**
+	 *  Finds the Most significant path for the whole ontology. 
+	 * @param nodeMatrix
+	 * @param alignmentMappings
+	 * @return
+	 */
+	public static List<ExplanationNode> findUniversalMostSignificantPath(ExplanationNode[][] nodeMatrix, Alignment<Mapping> alignmentMappings){
+		Map<ExplanationNode,Integer> trafficMap = new HashMap<ExplanationNode, Integer>();
+		for(Mapping m:alignmentMappings){
+			ExplanationNode explanationNode = nodeMatrix[m.getEntity1().getIndex()][m.getEntity2().getIndex()];
+			List<ExplanationNode> significantPathForNode = ExplanationNode.findMostSignificantPath(explanationNode);
+			for(ExplanationNode node:significantPathForNode){
+				if(!trafficMap.containsKey(node)){
+					trafficMap.put(node, 1);
+				}
+				else{
+					int newValue = trafficMap.get(node) + 1;
+					trafficMap.put(node,newValue);
+				}
+			}
+		}
+		System.out.println("The final significantPathValues are:");
+		for(ExplanationNode key: trafficMap.keySet()){
+			System.out.println("Key= "+key.description+" value="+trafficMap.get(key));
+		}
+		return null;
+	}
+
+
+
+
+	public ExplanationNode[][] getExplanationMatrix() {
+		return explanationMatrix;
+	}
+
+
+	public void setExplanationMatrix(ExplanationNode[][] explanationMatrix) {
+		this.explanationMatrix = explanationMatrix;
+	}
 	
 }
