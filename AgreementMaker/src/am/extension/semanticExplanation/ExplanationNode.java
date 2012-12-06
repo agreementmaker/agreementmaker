f74eae50c310103a27687014b02027b9737ead24
Jeevan Joseph <jjosep37@uic.edu>
2012-12-06 04:17:40 +0000

    SemanticExpln v2.0 Added universalMostSignificantPath to explanation node. To be moved to SemanticExpln

--- AgreementMaker/src/am/extension/semanticExplanation/ExplanationNode.java
+++ AgreementMaker/src/am/extension/semanticExplanation/ExplanationNode.java
@@ -1,10 +1,16 @@
 package am.extension.semanticExplanation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
+import org.apache.commons.collections.map.HashedMap;
+
+import am.app.mappingEngine.Alignment;
+import am.app.mappingEngine.Mapping;
 import edu.uci.ics.jung.graph.DelegateTree;
 
 public class ExplanationNode {
@@ -129,8 +135,8 @@ public class ExplanationNode {
 	}
 	
 	/**
-	 * @param ExplanationNode node
 	 * Finds the most significant path in an aligned mapping
+	 * @param ExplanationNode node
 	 * @return List of nodes
 	 */
 	public static List<ExplanationNode> findMostSignificantPath(ExplanationNode node) {
@@ -154,6 +160,34 @@ public class ExplanationNode {
 		}
 		return mspList;
 		
+	}
+	
+	/**
+	 *  Finds the Most significant path for the whole ontology. 
+	 * @param nodeMatrix
+	 * @param alignmentMappings
+	 * @return
+	 */
+	public static List<ExplanationNode> findUniversalMostSignificantPath(ExplanationNode[][] nodeMatrix, Alignment<Mapping> alignmentMappings){
+		Map<ExplanationNode,Integer> trafficMap = new HashMap<ExplanationNode, Integer>();
+		for(Mapping m:alignmentMappings){
+			ExplanationNode explanationNode = nodeMatrix[m.getEntity1().getIndex()][m.getEntity2().getIndex()];
+			List<ExplanationNode> significantPathForNode = findMostSignificantPath(explanationNode);
+			for(ExplanationNode node:significantPathForNode){
+				if(!trafficMap.containsKey(node)){
+					trafficMap.put(node, 1);
+				}
+				else{
+					int newValue = trafficMap.get(node) + 1;
+					trafficMap.put(node,newValue);
+				}
+			}
+		}
+		System.out.println("The final significantPathValues are:");
+		for(ExplanationNode key: trafficMap.keySet()){
+			System.out.println("Key= "+key.description+" value="+trafficMap.get(key));
+		}
+		return null;
 	}
 	
 	/**
