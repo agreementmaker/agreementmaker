743e3a6f9e5ce1935d34eaa0ed30675e66f8fca9
Pavan Reddy <preddy4@uic.edu>
2012-12-07 02:15:44 +0000

    cleaning up..

--- AgreementMaker/src/am/evaluation/repairExtended/KeyValue.java
+++ AgreementMaker/src/am/evaluation/repairExtended/KeyValue.java
@@ -8,19 +8,11 @@ public class KeyValue<K,V> {
 
 	private K Key;
 	private V Value;
-	//private Integer Flag;
 	
 	public KeyValue(K key, V value){
 		Key = key;
 		Value = value;
-		//Flag = 0;
 	}
-	
-	/*public KeyValue(K key, V value, Integer flag){
-		Key = key;
-		Value = value;
-		Flag = flag;
-	}*/
 	
 	public void setKey (K key)
     {
@@ -38,14 +30,5 @@ public class KeyValue<K,V> {
     public V getValue()
     {
         return Value;
-    }	
-    
-    /*public void setFlag (Integer flag)
-    {
-    	Flag = flag;           
     }
-    public Integer getFlag()
-    {
-        return Flag;
-    }*/
 }
