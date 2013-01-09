33280f0268a0979da734d475442bd51044279d40
Cosmin Stroe <cstroe@gmail.com>
2013-01-09 18:33:34 +0000

    Converted AgreementMaker project to Bundle.

--- AgreementMaker/src/am/app/osgi/AMActivator.java
+++ AgreementMaker/src/am/app/osgi/AMActivator.java
@@ -4,21 +4,22 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class AMActivator implements BundleActivator{
+public class AMActivator implements BundleActivator {
 
 	private BundleContext context;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		this.context=context;
-		
+		System.out.println("AgreementMaker Activator started...");
 	}
 
 	@Override
 	public void stop(BundleContext arg0) throws Exception {
 		this.context=null;
-		
+		System.out.println("AgreementMaker Activator stopped...");
 	}
+	
     public Bundle[] getBundles()
     {
         if (context != null)
