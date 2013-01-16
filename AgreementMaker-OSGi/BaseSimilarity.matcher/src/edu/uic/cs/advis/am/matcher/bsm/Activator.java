07e3b1f7526e726f4dd37247808d36e5cd97b82e
Cosmin Stroe <cstroe@gmail.com>
2013-01-16 15:56:18 +0000

    Register matcher as a service.

--- AgreementMaker-OSGi/BaseSimilarity.matcher/src/edu/uic/cs/advis/am/matcher/bsm/Activator.java
+++ AgreementMaker-OSGi/BaseSimilarity.matcher/src/edu/uic/cs/advis/am/matcher/bsm/Activator.java
@@ -1,7 +1,12 @@
 package edu.uic.cs.advis.am.matcher.bsm;
 
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import am.app.mappingEngine.AbstractMatcher;
 
 public class Activator implements BundleActivator {
 
@@ -11,6 +16,8 @@ public class Activator implements BundleActivator {
 		return context;
 	}
 
+	private ServiceRegistration<AbstractMatcher> reg;
+
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
@@ -18,6 +25,7 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 		System.out.println("Base Similarity Bundle started...");
+		reg = bundleContext.registerService(AbstractMatcher.class, new BaseSimilarityMatcher(), new Hashtable<String,String>());
 	}
 
 	/*
@@ -26,6 +34,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		reg.unregister();
 		System.out.println("Base Similarity Bundle stopped...");
 	}
 
