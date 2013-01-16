bd7cb71d69348df477babb8913aa3933ee9f0c43
Cosmin Stroe <cstroe@gmail.com>
2013-01-16 23:43:05 +0000

    PRA matcher bundle.

---
+++ AgreementMaker-OSGi/Matcher-PRA/src/main/java/am/matcher/pra/Activator.java
@@ -0,0 +1,35 @@
+package am.matcher.pra;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println(bundleContext.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " starting...");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		System.out.println(bundleContext.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " stopping...");
+	}
+
+}
