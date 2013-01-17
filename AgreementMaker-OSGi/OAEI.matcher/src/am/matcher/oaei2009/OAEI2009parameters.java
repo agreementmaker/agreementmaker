feca1793c75e9710c5bda05143591e6bf7cf612b
Cosmin Stroe <cstroe@gmail.com>
2013-01-17 04:51:49 +0000

    Fixed error code.  Removed reference to BatchMode class.

--- AgreementMaker-OSGi/OAEI.matcher/src/am/matcher/oaei2009/OAEI2009parameters.java
+++ AgreementMaker-OSGi/OAEI.matcher/src/am/matcher/oaei2009/OAEI2009parameters.java
@@ -2,25 +2,23 @@ package am.matcher.oaei2009;
 
 import am.app.mappingEngine.DefaultMatcherParameters;
 import am.app.mappingEngine.referenceAlignment.ReferenceAlignmentMatcher;
-import am.batchMode.AnatomyTrack;
-import am.batchMode.BenchmarkTrack;
-import am.batchMode.TrackDispatcher;
 
 public class OAEI2009parameters extends DefaultMatcherParameters {
 	
 	private static final long serialVersionUID = -8100658976837894464L;
 	
-	public final static String BENCHMARKS = TrackDispatcher.BENCHMARK;
-	public final static String ANATOMY = TrackDispatcher.ANATOMY;
+	public final static String BENCHMARKS = "benchmarks";
+	public final static String ANATOMY = "anatomy";
+	public final static String CONFERENCE = "conference";
+	public final static String CONFERENCE_EXTENDED = "conference_extended";	
 	public final static String ANATOMY_PRA = "Anatomy with PRA";
 	public final static String ANATOMY_PRI = "PraIntegration";
-	public final static String CONFERENCE = TrackDispatcher.CONFERENCE;
 	public final static String WordnetNoUMLS = "WordNet and no UMLS";
 	public final static String BENCHMARKS_303_PRA = "Benchmarks 303 with PRA";
 	
 	public boolean useWordNet = false;
 	public boolean useUMLS = true;
-	public String partialReferenceFile = "";
+	public String partialReferenceFile = null;
 	public String format = "";
 	public String trackName = ""; // the name of the track that this OAEI2009 matcher is running on.
 	public boolean runningFromCommandline = false;
@@ -29,7 +27,7 @@ public class OAEI2009parameters extends DefaultMatcherParameters {
 	
 	public OAEI2009parameters(String track) {
 		trackName = track;
-		if(track.equals(TrackDispatcher.ANATOMY)){
+		if(track.equals(ANATOMY)){
 			useWordNet = false;
 			useUMLS = true;
 			partialReferenceFile = "";
@@ -39,15 +37,16 @@ public class OAEI2009parameters extends DefaultMatcherParameters {
 			// this is Flavio's Anatomy PRI.
 			useWordNet = false;
 			useUMLS = true;
-			partialReferenceFile = AnatomyTrack.PARTIAL_REFERENCE;
+			if( partialReferenceFile == null ) 
+				throw new RuntimeException("You need to set the partial reference file.");
 			format = ReferenceAlignmentMatcher.OAEI;
-		
 		}
 		// this is Angela's Track!
 		else if(track.equals(ANATOMY_PRA)){
 			useWordNet = false;
 			useUMLS = true;
-			partialReferenceFile = AnatomyTrack.PARTIAL_REFERENCE;
+			if( partialReferenceFile == null ) 
+				throw new RuntimeException("You need to set the partial reference file.");
 			format = ReferenceAlignmentMatcher.OAEI;
 		}
 		else if(track.equals(BENCHMARKS)){
@@ -71,7 +70,8 @@ public class OAEI2009parameters extends DefaultMatcherParameters {
 		else if(track.equals(BENCHMARKS_303_PRA)){
 			useWordNet = false;
 			useUMLS = false;
-			partialReferenceFile = BenchmarkTrack.TRACK_INPUT_DIR+"303/refalign.rdf";
+			if( partialReferenceFile == null ) 
+				throw new RuntimeException("You need to set the partial reference file.");
 			format = ReferenceAlignmentMatcher.OAEI;
 		}
 	}
