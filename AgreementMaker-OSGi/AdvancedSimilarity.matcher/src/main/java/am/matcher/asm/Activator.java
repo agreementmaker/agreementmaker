e94a66ad57f1021e188b35e35fa58bdbdd0c6562
Cosmin Stroe <cstroe@gmail.com>
2013-01-17 01:33:15 +0000

    Minor change to sysout.

--- AgreementMaker-OSGi/AdvancedSimilarity.matcher/src/main/java/am/matcher/asm/Activator.java
+++ AgreementMaker-OSGi/AdvancedSimilarity.matcher/src/main/java/am/matcher/asm/Activator.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
 import am.app.mappingEngine.AbstractMatcher;
@@ -24,7 +25,8 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		System.out.println("Advanced Similarity Bundle started...");
+		System.out.println(bundleContext.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " starting...");
 		reg = bundleContext.registerService(AbstractMatcher.class, new AdvancedSimilarityMatcher(), new Hashtable<String,String>());
 	}
 
@@ -35,7 +37,8 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 		reg.unregister();
-		System.out.println("Advanced Similarity Bundle stopped...");
+		System.out.println(bundleContext.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " stopping...");
 	}
 
 }
