5d142fd380cffa5c69d02d49be10ec48f929856b
Meriya Thomas <mthoma43@uic.edu>
2013-03-26 01:47:39 +0000

    Added custom matcher

---
+++ AgreementMaker-OSGi/src/main/java/am/matcher/custom/Activator.java
@@ -0,0 +1,41 @@
+package am.matcher.custom;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import am.app.mappingEngine.AbstractMatcher;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	private ServiceRegistration reg;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("My matcher with explanation Bundle started...");
+		reg = bundleContext.registerService(AbstractMatcher.class, new MyMatcher_Explanation(), new Hashtable<String,String>());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		reg.unregister();
+		System.out.println("My matcher with explanation Bundle stopped...");
+	}
+
+}
