974980eaea4556376daaefd1a844b7bceffa6718
Francesco Loprete <f.loprete87@gmail.com>
2013-08-26 20:14:04 +0000

    New Matcher

---
+++ AgreementMaker-OSGi/Matcher-Dissimilar/src/main/java/am/matcher/dissimilar/DissimilarMatcherParameters.java
@@ -0,0 +1,24 @@
+package am.matcher.dissimilar;
+
+import am.app.mappingEngine.DefaultMatcherParameters;
+import am.app.ontology.profiling.classification.OntologyClassifier.OAEI2011Configuration;
+
+/**
+ * This year's matching algorithm uses a completely automatic configuration. 
+ *
+ */
+public class DissimilarMatcherParameters extends DefaultMatcherParameters {
+
+	private static final long serialVersionUID = 5708419970641391711L;
+	
+	public boolean automaticConfiguration = true;
+	public OAEI2011Configuration selectedConfiguration = OAEI2011Configuration.GENERAL_PURPOSE;
+	
+	public boolean showIntermediateMatchers = false;
+	public boolean parallelExecution = false;
+		
+	public DissimilarMatcherParameters() { 
+		super(); 
+	}
+	
+}
