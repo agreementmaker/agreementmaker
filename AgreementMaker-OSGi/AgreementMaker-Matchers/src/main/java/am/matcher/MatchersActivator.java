0a12e37de73830c1e3805c319c5ccdc9a65b18fb
Francesco Loprete <f.loprete87@gmail.com>
2013-08-29 16:36:16 +0000

    Add: Activator for 4 matchers

--- AgreementMaker-OSGi/AgreementMaker-Matchers/src/main/java/am/matcher/MatchersActivator.java
+++ AgreementMaker-OSGi/AgreementMaker-Matchers/src/main/java/am/matcher/MatchersActivator.java
@@ -1,7 +1,17 @@
 package am.matcher;
 
+import java.util.Hashtable;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+
+import am.app.mappingEngine.AbstractMatcher;
+import am.matcher.Combination.CombinationMatcher;
+import am.matcher.parametricStringMatcher.ParametricStringMatcher;
+import am.matcher.multiWords.MultiWordsMatcher;
+import am.matcher.LexicalSynonymMatcher.LexicalSynonymMatcher;
 
 public class MatchersActivator implements BundleActivator {
 
@@ -10,13 +20,22 @@ public class MatchersActivator implements BundleActivator {
 	static BundleContext getContext() {
 		return context;
 	}
-
+	private ServiceRegistration<AbstractMatcher> regCombination;
+	private ServiceRegistration<AbstractMatcher> regParametricString;
+	private ServiceRegistration<AbstractMatcher> regMultiWords;
+	private ServiceRegistration<AbstractMatcher> regLexicalSynonym;
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		MatchersActivator.context = bundleContext;
+		System.out.println(context.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " starting...");
+		regCombination = context.registerService(AbstractMatcher.class, new CombinationMatcher(), new Hashtable<String,String>());
+		regParametricString = context.registerService(AbstractMatcher.class, new ParametricStringMatcher(), new Hashtable<String,String>());
+		regMultiWords = context.registerService(AbstractMatcher.class, new MultiWordsMatcher(), new Hashtable<String,String>());
+		regLexicalSynonym = context.registerService(AbstractMatcher.class, new LexicalSynonymMatcher(), new Hashtable<String,String>());
 	}
 
 	/*
@@ -25,6 +44,13 @@ public class MatchersActivator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		MatchersActivator.context = null;
+		regCombination.unregister();
+		regParametricString.unregister();
+		regMultiWords.unregister();
+		regLexicalSynonym.unregister();
+		
+		System.out.println(context.getBundle().getHeaders().get
+				(Constants.BUNDLE_NAME) + " stopping...");
 	}
 
 }
