aebfb17a1a9018ec8818e21eb40a94c916201a06
Cosmin Stroe <cstroe@gmail.com>
2013-11-01 21:21:25 +0000

    Added the skeleton for a JUnit Test on this bundle.

---
+++ AgreementMaker-OSGi/Matcher-OAEI/src/test/java/am/matcher/oaei2009/OAEI2009MatcherTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package am.matcher.oaei2009;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author cosmin
+ *
+ */
+public class OAEI2009MatcherTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link am.matcher.oaei2009.OAEI2009matcher#match()}.
+	 */
+	@Test
+	public void testMatch() {
+		//fail("Not yet implemented");
+	}
+
+}
