40aeda26345b90f7c39c9815d9628586c5ee4439
Cosmin Stroe <cstroe@gmail.com>
2013-11-01 18:02:16 +0000

    Renamed activator.

--- AgreementMaker-OSGi/Matcher-OAEI/src/am/matcher/oaei/OAEIMatcherBundleActivator.java
+++ AgreementMaker-OSGi/Matcher-OAEI/src/am/matcher/oaei/OAEIMatcherBundleActivator.java
@@ -12,7 +12,14 @@ import am.matcher.oaei2009.OAEI2009matcher;
 import am.matcher.oaei2010.OAEI2010Matcher;
 import am.matcher.oaei2011.OAEI2011Matcher;
 
-public class Activator implements BundleActivator {
+/**
+ * This is the OAEI Matchers Bundle Activator. It will register the OAEI
+ * matchers contained in this bundle.
+ * 
+ * @author Cosmin Stroe
+ * 
+ */
+public class OAEIMatcherBundleActivator implements BundleActivator {
 
 	private static BundleContext context;
 
@@ -24,12 +31,8 @@ public class Activator implements BundleActivator {
 	private ServiceRegistration<AbstractMatcher> regOAEI2010;
 	private ServiceRegistration<AbstractMatcher> regOAEI2011;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+		OAEIMatcherBundleActivator.context = bundleContext;
 		System.out.println(bundleContext.getBundle().getHeaders().get
 				(Constants.BUNDLE_NAME) + " starting...");
 		regOAEI2009 = bundleContext.registerService(AbstractMatcher.class, new OAEI2009matcher(), new Hashtable<String,String>());
@@ -37,17 +40,13 @@ public class Activator implements BundleActivator {
 		regOAEI2011 = bundleContext.registerService(AbstractMatcher.class, new OAEI2011Matcher(), new Hashtable<String,String>());
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
 		regOAEI2009.unregister();
 		regOAEI2010.unregister();
 		regOAEI2011.unregister();
 		System.out.println(bundleContext.getBundle().getHeaders().get
 				(Constants.BUNDLE_NAME) + " stopping...");
+		OAEIMatcherBundleActivator.context = null;
 	}
 
 }
