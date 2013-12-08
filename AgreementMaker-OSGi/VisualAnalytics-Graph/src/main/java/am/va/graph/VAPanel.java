9fa13d56675d7e6a57691568791608e1ea351e48
Yanzi Jin <yjin25@uic.edu>
2013-12-08 22:41:47 +0000

    Change print out into functions, add more comments

--- AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAPanel.java
+++ AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAPanel.java
@@ -8,27 +8,15 @@ import javax.swing.JFrame;
 import com.sun.media.jfxmedia.events.NewFrameEvent;
 
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.Slider;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.ToolBarBuilder;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBoxBuilder;
 
 public class VAPanel {
 
@@ -41,15 +29,12 @@ public class VAPanel {
 	private static int count = 1;
 	private static int stop = -1;
 
-	private static int debug = 0;
-
 	/**
 	 * Init Frame
 	 */
 	public static void initAndShowGUI() {
 		frame = new JFrame("VA");
-		frame.setSize(1100, 550);
-		frame.setLocation(100, 100);
+		frame.setSize(500, 500);
 		fxPanel = new JFXPanel();
 		frame.add(fxPanel);
 		frame.setVisible(true);
@@ -67,62 +52,29 @@ public class VAPanel {
 	 */
 	public static void InitFX() {
 		root = new Group();
-
-		Scene myScene = new Scene(root);
-
-		// Main layout: BorderPane
-		BorderPane borderPane = new BorderPane();
-
-		// left side: listView
 		listView = new ListView<String>();
+		final Scene myScene = new Scene(root);
+		final VAPieChart chart = new VAPieChart(rootGroup);
+
+		chart.getPieChart().setClockwise(false);
+		root.getChildren().add(chart.getPieChart());
+		root.getChildren().add(listView);
+		//listView.setVisible(false);
 		listView.setPrefHeight(500);
 		listView.setPrefWidth(100);
-		borderPane.setLeft(listView);
+		listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-		// Top side: HBox, contains toolbar
-		ToolBar toolbar = new ToolBar();
-		Region spacer = new Region();
-		spacer.setStyle("-fx-padding: 0 7em 0 0;");
-		spacer.getStyleClass().setAll("spacer");
-		HBox buttonBar = new HBox();
-		Button btn1 = new Button("Home");
-		Button btn2 = new Button("Option");
-		Button btn3 = new Button("Help");
-		buttonBar.getChildren().addAll(btn1, btn2, btn3);
-		toolbar.getItems().addAll(spacer, buttonBar);
-		borderPane.setTop(toolbar);
-
-		// Center side: two piecharts as a group, tilepane layout is used
-		Group chartGroup = new Group();
-		TilePane tilePane = new TilePane();
-		tilePane.setPrefColumns(2); // preferred columns
-		VAPieChart chartLeft = new VAPieChart(rootGroup);
-		chartLeft.getPieChart().setClockwise(false);
-		PieChart chartRight = testPieChart();
-		tilePane.getChildren().add(chartLeft.getPieChart());
-		tilePane.getChildren().add(chartRight);
-		chartGroup.getChildren().add(tilePane);
-		borderPane.setCenter(chartGroup);
-
-		root.getChildren().add(borderPane);
 		fxPanel.setScene(myScene);
 		updateCurrentGroup(rootGroup);
-		setLocation(chartLeft);
-		// TEST(currentGroup);
-		chartLeft.updatePieChart();
+		setLocation(chart);
+		testVAGroup(currentGroup);
+		chart.updatePieChart();
 	}
 
-	private static PieChart testPieChart() {
-		ObservableList<PieChart.Data> pieChartData = FXCollections
-				.observableArrayList(new PieChart.Data("Sun", 20),
-						new PieChart.Data("IBM", 12), new PieChart.Data("HP",
-								25), new PieChart.Data("Dell", 22),
-						new PieChart.Data("Apple", 30));
-		PieChart chart = new PieChart(pieChartData);
-		chart.setClockwise(false);
-		return chart;
-	}
-
+	/**
+	 * Set radius and center point for Pie chart
+	 * @param chart
+	 */
 	private static void setLocation(VAPieChart chart) {
 		// TODO Auto-generated method stub
 		double minX = Double.MAX_VALUE;
@@ -140,9 +92,8 @@ public class VAPanel {
 		double radius = (maxX - minX) / 2;
 		chart.setRadius(radius);
 		chart.setPieCenter(new Point2D(minX + radius, minY + radius));
-		if (debug == 1)
-			System.out.println("radius " + radius + " center "
-					+ chart.getPieCenter());
+		System.out.println("radius " + radius + " center "
+				+ chart.getPieCenter());
 	}
 
 	/**
@@ -153,12 +104,14 @@ public class VAPanel {
 	 */
 	public static void getNewGroup(VAGroup currentGroup) {
 		// Need a function here, return value:VAData
-		VAData newRootData;
-		System.out.println(count);
+		VAData newRootData = VAPieChart.getSelectedVAData();
+		System.out.println("New data " + newRootData.getSourceNode().getLocalName());
+		/*System.out.println(count);
 		if (count == 1)
-			newRootData = currentGroup.getVADataArray().get(4);
-		else
 			newRootData = currentGroup.getVADataArray().get(3);
+		else
+			newRootData = currentGroup.getVADataArray().get(1);
+		*/
 		count++;
 		if (newRootData != null
 				&& newRootData.getSourceNode().getChildCount() > 0) { // if
@@ -178,7 +131,6 @@ public class VAPanel {
 
 	/**
 	 * Update current group
-	 * 
 	 * @param group
 	 */
 	private static void updateCurrentGroup(VAGroup group) {
@@ -203,10 +155,9 @@ public class VAPanel {
 
 	/**
 	 * Print info. Testing only.
-	 * 
 	 * @param rootGroup
 	 */
-	public static void TEST(VAGroup rootGroup) {
+	public static void testVAGroup(VAGroup rootGroup) {
 		String rootNodeName = rootGroup.getRootNode().getSourceNode()
 				.getLocalName();
 		System.out.println(rootNodeName);
@@ -217,10 +168,9 @@ public class VAPanel {
 					+ d.getSimilarity());
 		}
 		HashMap<String, Integer> slots = rootGroup.getslotCountMap();
-		if (debug == 0)
-			for (String s : slots.keySet()) {
-				System.out.println(s + ":" + slots.get(s));
-			}
+		for (String s : VAVariables.thresholdName) {
+			System.out.println(s + ":" + slots.get(s));
+		}
 	}
 
 	public static VAGroup getCurrentGroup() {
