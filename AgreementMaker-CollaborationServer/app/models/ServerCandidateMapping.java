bf9ff6f53d09ed1edda47986269e03cebbc21960
Cosmin Stroe <cstroe@gmail.com>
2013-12-11 19:21:06 +0000

    Added JPA annotations.

--- AgreementMaker-CollaborationServer/app/models/ServerCandidateMapping.java
+++ AgreementMaker-CollaborationServer/app/models/ServerCandidateMapping.java
@@ -1,8 +1,15 @@
 package models;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.Constraint;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import play.data.validation.Constraints;
 import play.db.ebean.Model;
@@ -13,6 +20,7 @@ public class ServerCandidateMapping extends Model {
 	private static final long serialVersionUID = 184274387042579061L;
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) // the id is unique only per type hierarchy
 	public Long id;
 	
 	@Constraints.Required
@@ -20,9 +28,17 @@ public class ServerCandidateMapping extends Model {
 	
 	@Constraints.Required
 	public String targetURI;
-	
 
-	public String feedback;
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date timeSent;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date timeReceived;
+	
+	public enum FeedbackType { CORRECT, INCORRECT, SKIP, END_EXPERIMENT }
+	
+	@Enumerated(EnumType.STRING)
+	public FeedbackType feedback;
 	
 	public static Model.Finder<Long,ServerCandidateMapping> find = 
     		new Model.Finder<Long,ServerCandidateMapping>(Long.class, ServerCandidateMapping.class);
