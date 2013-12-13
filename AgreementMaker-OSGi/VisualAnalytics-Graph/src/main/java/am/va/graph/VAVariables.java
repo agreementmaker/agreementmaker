0ce508a66330fa9dbc02e8b50d07fc7ded5d6cb8
Yiting Li <yli229@uic.edu>
2013-12-13 04:46:56 +0000

    current version

--- AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAVariables.java
+++ AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAVariables.java
@@ -3,7 +3,7 @@ package am.va.graph;
 import java.util.HashMap;
 
 public interface VAVariables {
-	int DEBUG = 1;
+	int DEBUG = 0;
 	int slotNum = 5;
 	double threshold[] = { 0.00, 0.20, 0.40, 0.60, 0.80, 1.00 };
 	String thresholdName[] = { "0-20%", "20-40%", "40-60%", "60-80%", "80-100%" };
