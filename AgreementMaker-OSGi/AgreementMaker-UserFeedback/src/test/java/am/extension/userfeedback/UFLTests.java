9578e792ee0754d03e12583375b37829c6d08f9f
Cosmin Stroe <cstroe@gmail.com>
2014-02-05 19:46:15 +0000

    do not create the tests.

--- AgreementMaker-OSGi/AgreementMaker-UserFeedback/src/test/java/am/extension/userfeedback/UFLTests.java
+++ AgreementMaker-OSGi/AgreementMaker-UserFeedback/src/test/java/am/extension/userfeedback/UFLTests.java
@@ -42,7 +42,7 @@ public class UFLTests {
 	private static final String BM303_CS_STATIC_DATA = "UFL/experiments/UnitTestData/303-static-cs.data.xml";
 	private static final String BM304_CS_STATIC_DATA = "UFL/experiments/UnitTestData/304-static-cs.data.xml";
 	
-	private static final boolean CREATE_FILES = true;
+	private static final boolean CREATE_FILES = false;
 	
 	@Test
 	public void testBenchmarks301() {
