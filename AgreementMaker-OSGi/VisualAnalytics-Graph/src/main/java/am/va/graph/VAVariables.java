7918c75f3e577ff2e48ba553ee82ba4c7e39907c
Yiting Li <yli229@uic.edu>
2014-03-08 22:51:53 +0000

    Static color for pie charts.

--- AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAVariables.java
+++ AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAVariables.java
@@ -1,5 +1,6 @@
 package am.va.graph;
 
+import java.util.HashMap;
 
 public interface VAVariables {
 	int DEBUG = 0;
@@ -11,6 +12,16 @@ public interface VAVariables {
 			"Not matched" };
 	String selectionStyle[] = { "#FFD700", "#90EE90", "#B0E0E6", "#FFC0CB",
 			"#FF6347" };
+
+	HashMap<String, String> ColorRange = new HashMap<String, String>() {
+		{
+			put("0-20%", "#ffcc99");
+			put("20-40%", "#ffccff");
+			put("40-60%", "#66b2ff");
+			put("60-80%", "#ff66ff");
+			put("80-100%", "#ffff33");
+		}
+	};
 
 	enum nodeType {
 		Class, Property;
