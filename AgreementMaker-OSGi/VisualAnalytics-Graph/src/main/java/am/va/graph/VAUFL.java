15a966167817a3de818f79dcbd6b0c9b3353d578
Yiting Li <yli229@uic.edu>
2014-03-20 00:27:57 +0000

    Need to find out all possible mappings for each source concept/property

--- AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAUFL.java
+++ AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAUFL.java
@@ -3,8 +3,10 @@ package am.va.graph;
 import java.util.List;
 
 import am.app.Core;
-import am.app.mappingEngine.MatcherResult;
 import am.app.mappingEngine.MatchingTask;
+import am.app.mappingEngine.manualMatcher.UserManualMatcher;
+import am.app.mappingEngine.similarityMatrix.SimilarityMatrix;
+import am.app.ontology.Ontology;
 
 public class VAUFL {
 	List<MatchingTask> matchingTask;
@@ -13,14 +15,25 @@ public class VAUFL {
 	public VAUFL() {
 		matchingTask = Core.getInstance().getMatchingTasks();
 		userTask = matchingTask.get(0);
+		setBestMatchingGroup();
 	}
 
+	/**
+	 * Set the user manual matcher to the best matching task
+	 * 
+	 * @return
+	 */
 	private boolean setBestMatchingGroup() {
-		int len = matchingTask.size() - 1;
+		int len = matchingTask.size();
 		int res = 0, best = -1;
-		for (int i = 0; i < len; i++) {
+		for (int i = 1; i < len; i++) {
 			MatchingTask m = matchingTask.get(i);
-			int tmp = m.selectionResult.classesAlignment.size() + m.selectionResult.propertiesAlignment.size();
+			int tmp = 0;
+			if (m.selectionResult != null && m.selectionResult.classesAlignment != null)
+				tmp += m.selectionResult.classesAlignment.size();
+			if (m.selectionResult != null && m.selectionResult.propertiesAlignment != null)
+				tmp += m.selectionResult.propertiesAlignment.size();
+			// System.out.println("number of alignments=" + tmp);
 			if (tmp > res) {
 				best = i;
 				res = tmp;
@@ -28,11 +41,42 @@ public class VAUFL {
 		}
 		if (best == -1)
 			return false;
-		MatcherResult userResult = userTask.matcherResult;
-		//copy the 'best' result to userResult
+		// update the 'best' result to userResult
+		updateUserTask(matchingTask.get(best));
 		return true;
 	}
-	
-	
-	
+
+	/**
+	 * Update the user manual matcher, called by setBestMatchingGroup()
+	 * 
+	 * @param bestTest
+	 */
+	private void updateUserTask(MatchingTask bestTest) {
+
+		// DefaultMatcherParameters mParam = new DefaultMatcherParameters();
+
+		UserManualMatcher m = new UserManualMatcher();
+		m.setSourceOntology(Core.getInstance().getSourceOntology());
+		m.setTargetOntology(Core.getInstance().getTargetOntology());
+
+		userTask = new MatchingTask(m, bestTest.matcherParameters, bestTest.selectionAlgorithm,
+				bestTest.selectionParameters);
+
+		userTask.match();
+		userTask.select();
+		// System.out.println("set best matcher: class=" +
+		// userTask.selectionResult.classesAlignment.size());
+		// System.out.println("set best matcher: properity=" +
+		// userTask.selectionResult.propertiesAlignment.size());
+	}
+
+	private void getAbiMatchings(VAVariables.ontologyType type) {
+		// iterate the source ontology concepts
+		int len = matchingTask.size();
+		SimilarityMatrix sMatrix;
+		for (int i = 1; i <= len; i++) {
+			if (type == VAVariables.ontologyType.Source)
+				sMatrix = matchingTask.get(i).matcherResult.getClassesMatrix();
+		}
+	}
 }
