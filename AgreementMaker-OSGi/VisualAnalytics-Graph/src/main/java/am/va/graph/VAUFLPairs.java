11288e535ae35ea33fa9a39145f27c67686535b2
Yiting Li <yli229@uic.edu>
2014-04-30 17:20:40 +0000

    add label for questions

--- AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAUFLPairs.java
+++ AgreementMaker-OSGi/VisualAnalytics-Graph/src/main/java/am/va/graph/VAUFLPairs.java
@@ -6,16 +6,16 @@ import am.app.ontology.Node;
 
 /**
  * Possible alignment pairs, 1 to n
+ * 
  * @author Yiting
- *
+ * 
  */
 public class VAUFLPairs {
 	private Node sourceNode;
-	private HashMap<String, Node> targetNodes; //target name & target node
+	private HashMap<String, Node> targetNodes; // target name & target node
 	private String bestChoice;
 	private double sim;
-	
-	
+
 	public VAUFLPairs(Node sourceNode) {
 		super();
 		this.sourceNode = sourceNode;
@@ -23,14 +23,15 @@ public class VAUFLPairs {
 		this.bestChoice = "";
 		sim = 0;
 	}
-	
+
 	public Node getSourceNode() {
 		return sourceNode;
 	}
+
 	public void setSourceNode(Node sourceNode) {
 		this.sourceNode = sourceNode;
 	}
-	
+
 	public HashMap<String, Node> getTargetNodes() {
 		return targetNodes;
 	}
@@ -39,40 +40,40 @@ public class VAUFLPairs {
 		this.targetNodes = targetNodes;
 	}
 
-	public void addToTargetList(Node n){
-		targetNodes.put(n.getLocalName(), n);
+	public void addToTargetList(Node n) { // set name here
+		targetNodes.put(n.getLocalName() + "|" + n.getLabel(), n);
 	}
-	
-	public void setBestChoice(String c){
+
+	public void setBestChoice(String c) {
 		this.bestChoice = c;
 	}
-	
-	public boolean selected(){
+
+	public boolean selected() {
 		return !bestChoice.equals("");
 	}
-	
-	public String getBestChoice(){
+
+	public String getBestChoice() {
 		return bestChoice;
 	}
-	
-	public void setSim(double sim){
+
+	public void setSim(double sim) {
 		this.sim = sim;
 	}
-	
-	public String getSim(){
+
+	public String getSim() {
 		return String.valueOf(sim);
 	}
-	
+
 	/**
 	 * Check if target node is already in the hash map
+	 * 
 	 * @param target
 	 * @return
 	 */
-	public boolean containTarget(String target){
-		if(targetNodes.containsKey(target))
+	public boolean containTarget(String target) {
+		if (targetNodes.containsKey(target))
 			return true;
 		return false;
 	}
-	
-	
+
 }
