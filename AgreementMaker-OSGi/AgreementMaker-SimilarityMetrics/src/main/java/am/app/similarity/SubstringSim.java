90345047051c98677934eaf2d7a621b3856cd7b4
Cosmin Stroe <cstroe@gmail.com>
2016-10-13 23:10:10 -0500

    Add tests against, and refactor SubstringSim

--- AgreementMaker-OSGi/AgreementMaker-SimilarityMetrics/src/main/java/am/app/similarity/SubstringSim.java
+++ AgreementMaker-OSGi/AgreementMaker-SimilarityMetrics/src/main/java/am/app/similarity/SubstringSim.java
@@ -16,30 +16,34 @@ public class SubstringSim implements StringSimilarityMeasure {
 	// return the sum of common characters in all
 	// substrings of the two words normalized by the number of total chars
 	// So this is only the commonality part is not the whole I-Sub method
-	public static double substringScore(String s1, String s2)
-	{
-
-		int l1 = s1.length(), l2 = s2.length();
-		int L1 = l1, L2 = l2;
+	public static double substringScore(final String string1, final String string2) {
 		double common = 0;
 		int best = 2;
-		while (s1.length() > 0 && s2.length() > 0 && best != 0) {
+
+        String currentString1 = string1;
+        String currentString2 = string2;
+		while (currentString1.length() > 0 && currentString2.length() > 0 && best != 0) {
 			best = 0;
-			l1 = s1.length();
-			l2 = s2.length();
-			int i = 0, j = 0;
+			final int currentLength1 = currentString1.length();
+			final int currentLength2 = currentString2.length();
 			int startS1 = 0, endS1 = 0;
 			int startS2 = 0, endS2 = 0;
-			int p = 0;
-			for (i = 0; (i < l1) && (l1 - i > best); i++) {
+			for (int i = 0, j; i < currentLength1 && currentLength1 - i > best; i++) {
 				j = 0;
-				while (l2 - j > best) {
+				while (currentLength2 - j > best) {
 					int k = i;
-					for (; (j < l2) && (s1.charAt(k) != s2.charAt(j)); j++) {}
-					if (j != l2) {
-						p = j;
-						for (j++, k ++; (j < l2) && (k < l1)
-								&& (s1.charAt(k) == s2.charAt(j)); j++, k++) {}
+					while(j < currentLength2 && currentString1.charAt(k) != currentString2.charAt(j)) {
+					    j++;
+					}
+					if (j != currentLength2) {
+						int p = j;
+                        j++;
+                        k++;
+						while (j < currentLength2 && k < currentLength1 &&
+                                currentString1.charAt(k) == currentString2.charAt(j)) {
+						    j++;
+                            k++;
+                        }
 						if (k - i > best) {
 							best = k - i;
 							startS1 = i;
@@ -50,35 +54,21 @@ public class SubstringSim implements StringSimilarityMeasure {
 					}
 				}
 			}
-            char[] newString = new char[s1.length() - (endS1 - startS1)];
-			j = 0;
-			for (i = 0; i < s1.length(); i++) {
-				if (i >= startS1 && i < endS1) {
-					continue;
-				}
-				newString[j++] = s1.charAt(i);
-			}
-			s1 = new String(newString);
-			newString = new char[s2.length() - (endS2 - startS2)];
-			j = 0;
-			for (i = 0; i < s2.length(); i++) {
-				if (i >= startS2 && i < endS2) {
-					continue;
-				}
-				newString[j++] = s2.charAt(i);
-			}
-			s2 = new String(newString);
-			if (best > 2) {
+
+            currentString1 = excludeRange(currentString1, startS1, endS1);
+            currentString2 = excludeRange(currentString2, startS2, endS2);
+
+            if (best > 2) {
 				common += best;
 			} else {
 				best = 0;
 			}
 		}
-		double commonality = 0;
-		double scaledCommon = (double) (2 * common) / (L1 + L2);
-		commonality = scaledCommon;
 
-		return commonality;
+        return (2 * common) / (string1.length() + string2.length());
 	}
 
+	static String excludeRange(String currentString1, int startS1, int endS1) {
+	    return currentString1.substring(0, startS1) + currentString1.substring(endS1);
+    }
 }
--- AgreementMaker-OSGi/AgreementMaker-SimilarityMetrics/src/test/java/am/app/similarity/SubstringSimTest.java
+++ AgreementMaker-OSGi/AgreementMaker-SimilarityMetrics/src/test/java/am/app/similarity/SubstringSimTest.java
@@ -1,11 +1,13 @@
 package am.app.similarity;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SubstringSimTest {
+    private static final double PRECISION = 0.000_000_001;
     private SubstringSim metric;
 
     @Before
@@ -30,8 +32,81 @@ public class SubstringSimTest {
 
     @Test
     public void equal_strings_have_exact_similarity() {
-        assertEquals(1d, metric.getSimilarity("", ""), 0.000_000_001);
-        assertEquals(1d, metric.getSimilarity("abcdefg", "abcdefg"), 0.000_000_001);
-        assertEquals(1d, metric.getSimilarity("12345", "12345"), 0.000_000_001);
+        assertEquals(1d, metric.getSimilarity("", ""), PRECISION);
+        assertEquals(1d, metric.getSimilarity("abcdefg", "abcdefg"), PRECISION);
+        assertEquals(1d, metric.getSimilarity("12345", "12345"), PRECISION);
+    }
+
+    @Test
+    public void exclude_range() {
+        String input = "aninputstring";
+        assertEquals("astring", SubstringSim.excludeRange(input, 1, 7));
+    }
+
+    @Test
+    public void no_substring() {
+        assertEquals(0d, metric.getSimilarity("a", ""), PRECISION);
+        assertEquals(0d, metric.getSimilarity("", "b"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("a", "b"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("a", "bcdefghijk"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("abcdefghijk", "l"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("abcdefghijk", "lmnopqrst"), PRECISION);
+    }
+
+    @Test
+    public void single_substring() {
+        assertEquals(12d/14, metric.getSimilarity("astring", "bstring"), PRECISION);
+        assertEquals(12d/14, metric.getSimilarity("stringa", "bstring"), PRECISION);
+    }
+
+    @Test
+    public void single_character_is_not_considered_a_substring_match() {
+        assertEquals(0d, metric.getSimilarity("a", "astring"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("a", "straing"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("a", "stringa"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("astring", "a"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("straing", "a"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("stringa", "a"), PRECISION);
+    }
+
+    @Test
+    public void two_character_substring_is_not_considered_a_substring_match() {
+        assertEquals(0d, metric.getSimilarity("as", "astring"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("as", "tringas"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("as", "triasng"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("astring", "as"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("tringas", "as"), PRECISION);
+        assertEquals(0d, metric.getSimilarity("triasng", "as"), PRECISION);
+    }
+
+    @Test
+    public void three_character_substring() {
+        assertEquals(6d/10, metric.getSimilarity("ast", "astring"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("ast", "ringast"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("ast", "riastng"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("astring", "ast"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("ringast", "ast"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("riastng", "ast"), PRECISION);
+
+        assertEquals(6d/14, metric.getSimilarity("riastng", "asterix"), PRECISION);
+        assertEquals(6d/14, metric.getSimilarity("asterix", "riastng"), PRECISION);
+    }
+
+    @Test
+    public void two_separate_substrings() {
+        assertEquals(12d/14, metric.getSimilarity("asterix", "astwrix"), PRECISION);
+        assertEquals(12d/14, metric.getSimilarity("astwrix", "asterix"), PRECISION);
+    }
+
+    @Test
+    public void repeated_substrings_twice() {
+        assertEquals(6d/10, metric.getSimilarity("ast", "astwast"), PRECISION);
+        assertEquals(6d/10, metric.getSimilarity("astwast", "ast"), PRECISION);
+    }
+
+    @Test
+    public void repeated_substrings_thrice() {
+        assertEquals(6d/14, metric.getSimilarity("ast", "astwastwast"), PRECISION);
+        assertEquals(6d/14, metric.getSimilarity("astwastwast", "ast"), PRECISION);
     }
 }
