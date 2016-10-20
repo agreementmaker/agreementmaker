ac771eb701e7891e138bbf72b9ed15e36d6164a5
Cosmin Stroe <cstroe@gmail.com>
2016-10-20 07:58:25 -0500

    Don't register matchers in the BundleActivator.

--- AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/osgi/AMActivator.java
+++ AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/osgi/AMActivator.java
@@ -1,54 +1,24 @@
 package am.app.osgi;
 
-import java.util.Hashtable;
-
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 
 import am.app.Core;
-import am.app.mappingEngine.AbstractMatcher;
-import am.app.mappingEngine.manualMatcher.UserManualMatcher;
-import am.app.mappingEngine.referenceAlignment.ReferenceAlignmentMatcher;
 
 public class AMActivator implements BundleActivator {
 
 	private BundleContext context;
-	
-	private ServiceRegistration<AbstractMatcher> regUserManualMatcher;
-	private ServiceRegistration<AbstractMatcher> regReferenceAlignmentMatcher;
-	
+
 	@Override
 	public void start(BundleContext context) throws Exception {
-		this.context=context;
-		System.out.println("AgreementMaker Core started...");
-		
-		//System.setProperty("apple.laf.useScreenMenuBar", "true");
-		//System.setProperty("apple.awt.brushMetalLook", "true");
-		
-		regUserManualMatcher = context.registerService(AbstractMatcher.class, new UserManualMatcher(), new Hashtable<String,String>());
-		regReferenceAlignmentMatcher = context.registerService(AbstractMatcher.class, new ReferenceAlignmentMatcher(), new Hashtable<String,String>());
-		
+		this.context = context;
 		Core.getInstance().initializeOSGiRegistry(context);
-		
+		System.out.println("AgreementMaker Core started...");
 	}
 
 	@Override
 	public void stop(BundleContext arg0) throws Exception {
-		regUserManualMatcher.unregister();
-		regReferenceAlignmentMatcher.unregister();
-		this.context=null;
+		this.context = null;
 		System.out.println("AgreementMaker Core stopped...");
 	}
-	
-    public Bundle[] getBundles()
-    {
-        if (context != null)
-        {
-            return context.getBundles();
-        }
-        return null;
-    } 
-
 }
