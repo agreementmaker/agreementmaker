d0f44cb90dcdc4a7d713925212c9242cf409dfff
Cosmin Stroe <cstroe@gmail.com>
2016-10-19 21:17:35 -0500

    AllOneMatcher uses the new API.

--- AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/mappingEngine/testMatchers/AllOneMatcher.java
+++ AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/mappingEngine/testMatchers/AllOneMatcher.java
@@ -1,12 +1,16 @@
 package am.app.mappingEngine.testMatchers;
 
+import am.api.matching.Matcher;
+import am.api.matching.MatcherResult;
+import am.api.ontology.Ontology;
 import am.app.mappingEngine.AbstractMatcher;
 import am.app.mappingEngine.Mapping;
 import am.app.mappingEngine.Mapping.MappingRelation;
 import am.app.mappingEngine.similarityMatrix.SimilarityMatrix;
 import am.app.ontology.Node;
+import am.ds.matching.MatcherResultImpl;
 
-public class AllOneMatcher extends AbstractMatcher {
+public class AllOneMatcher extends AbstractMatcher implements Matcher {
 	
 	private static final long serialVersionUID = 2854018267136437040L;
 
@@ -16,8 +20,16 @@ public class AllOneMatcher extends AbstractMatcher {
 		setName("AllOne Matcher");
 		setCategory(MatcherCategory.UTILITY);
 	}
-	
-	/**Set all alignment sim to 1*/
+
+    @Override
+    public MatcherResult match(Ontology source, Ontology target) {
+        return new MatcherResultImpl(
+                (sourceEntity, targetEntity) -> 1,
+                (sourceEntity, targetEntity) -> 1,
+                (sourceEntity, targetEntity) -> 1);
+    }
+
+    /**Set all alignment sim to 1*/
 	@Override
 	public Mapping alignTwoNodes(Node source, Node target, alignType typeOfNodes, SimilarityMatrix matrix) {
 		double sim = 1;
---
+++ AgreementMaker-OSGi/AgreementMaker-Core/test/am/app/mappingEngine/testMatchers/AllOneMatcherTest.java
@@ -0,0 +1,53 @@
+package am.app.mappingEngine.testMatchers;
+
+import am.api.matching.MatcherResult;
+import am.api.matching.SimilarityMatrix;
+import am.api.ontology.Class;
+import am.api.ontology.Ontology;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AllOneMatcherTest {
+    private Ontology o1;
+    private Ontology o2;
+
+    private List<Class> o1Classes = new ArrayList<>(4);
+    private List<Class> o2Classes = new ArrayList<>(4);
+
+    @Before
+    public void setUp() {
+        o1 = mock(Ontology.class);
+        o2 = mock(Ontology.class);
+
+        o1Classes = new ArrayList<>(4);
+        o2Classes = new ArrayList<>(4);
+        for(int i = 0; i < 4; i++) {
+            o1Classes.add(mock(Class.class));
+            o2Classes.add(mock(Class.class));
+        }
+
+        when(o1.getClasses()).thenReturn(o1Classes);
+        when(o2.getClasses()).thenReturn(o2Classes);
+    }
+
+    @Test
+    public void match_classes() {
+        AllOneMatcher matcher = new AllOneMatcher();
+        MatcherResult result = matcher.match(o1, o2);
+
+        SimilarityMatrix<Class> classSimilarityMatrix = result.getClasses().get();
+
+        for (Class o1Class : o1Classes) {
+            for (Class o2Class : o2Classes) {
+                assertEquals(classSimilarityMatrix.getSimilarity(o1Class, o2Class), 1d, 0.000001);
+            }
+        }
+    }
+}
