c75f350af12d16b0f29d9a30f360e7fb83f556e5
Cosmin Stroe <cstroe@gmail.com>
2016-10-20 08:17:23 -0500

    We're testing the Builder.

--- AgreementMaker-OSGi/AgreementMaker-API/src/test/java/am/api/matching/MatcherResultBuilderTest.java
+++ AgreementMaker-OSGi/AgreementMaker-API/src/test/java/am/api/matching/MatcherResultBuilderTest.java
@@ -1,14 +1,16 @@
-package am.ds.matching;
+package am.api.matching;
 
 import am.api.matching.MatcherResult;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MatcherResultImplTest {
+public class MatcherResultBuilderTest {
     @Test
     public void should_accept_null() {
-        MatcherResult result = new MatcherResultImpl(null, null, null);
+        MatcherResult result = new MatcherResult.Builder()
+                .build();
+
         assertFalse(result.getClasses().isPresent());
         assertFalse(result.getProperties().isPresent());
         assertFalse(result.getInstances().isPresent());
