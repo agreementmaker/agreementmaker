206e004a2437aff134a7c0fa2f0f3aafaaca7442
Cosmin Stroe <cstroe@gmail.com>
2016-10-21 07:06:43 -0500

    Make all the alignments optional.

--- AgreementMaker-OSGi/AgreementMaker-API/src/main/java/am/api/selector/SelectorResult.java
+++ AgreementMaker-OSGi/AgreementMaker-API/src/main/java/am/api/selector/SelectorResult.java
@@ -4,8 +4,10 @@ import am.api.ontology.Class;
 import am.api.ontology.Instance;
 import am.api.ontology.Property;
 
+import java.util.Optional;
+
 public interface SelectorResult {
-    Alignment<Class> getClassAlignment();
-    Alignment<Property> getPropertyAlignment();
-    Alignment<Instance> getInstanceAlignment();
+    Optional<Alignment<Class>> getClassAlignment();
+    Optional<Alignment<Property>> getPropertyAlignment();
+    Optional<Alignment<Instance>> getInstanceAlignment();
 }
