ee1072a462f1447d8dfc9d0f08287797a6e2d2fc
Cosmin Stroe <cstroe@gmail.com>
2016-11-21 07:53:13 -0600

    remove unnecessary qualifiers

--- AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/mappingEngine/qualityEvaluation/MappingQualityMetric.java
+++ AgreementMaker-OSGi/AgreementMaker-Core/src/am/app/mappingEngine/qualityEvaluation/MappingQualityMetric.java
@@ -1,22 +1,3 @@
-/**    ________________________________________
- * ___/ Copyright Notice / Warranty Disclaimer \________________
- *
- * @copyright { Copyright (c) 2010
- * Advances in Information Systems Laboratory at the
- * University of Illinois at Chicago
- * All Rights Reserved. }
- * 
- * @disclaimer {
- * This work is distributed WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. }
- * 
- *     _____________________
- * ___/ Authors / Changelog \___________________________________          
- * 
- *  
- */
-
 package am.app.mappingEngine.qualityEvaluation;
 
 import am.app.mappingEngine.AbstractMatcher;
@@ -25,8 +6,8 @@ import am.utility.parameters.HasParameters;
 
 public interface MappingQualityMetric extends HasParameters {
 	
-	public double getQuality(alignType type, int i, int j);
+	double getQuality(alignType type, int i, int j);
 	
-	public QualityEvaluationData getQuality( AbstractMatcher matcher ) throws Exception;
+	QualityEvaluationData getQuality( AbstractMatcher matcher ) throws Exception;
 	
 }
