c2cc688c33b474abeb1b44c50b7ed9cb6aa9a687
Cosmin Stroe <cstroe@gmail.com>
2018-10-31 02:06:51 -0500

    Fix regression.

--- AgreementMaker-OSGi/AgreementMaker-BatchMode/src/main/java/am/extension/batchmode/internal/providers/MatcherProviderFromClasspath.java
+++ AgreementMaker-OSGi/AgreementMaker-BatchMode/src/main/java/am/extension/batchmode/internal/providers/MatcherProviderFromClasspath.java
@@ -2,13 +2,16 @@ package am.extension.batchmode.internal.providers;
 
 import am.app.mappingEngine.AbstractMatcher;
 import am.extension.batchmode.api.BatchModeMatcherProvider;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MatcherProviderFromClasspath extends ProviderFromClasspath implements BatchModeMatcherProvider {
-    public MatcherProviderFromClasspath(String canonicalClassName) {
+    public MatcherProviderFromClasspath(@JsonProperty("canonicalClassName") String canonicalClassName) {
         super(canonicalClassName);
     }
 
     @Override
+    @JsonIgnore
     public AbstractMatcher getMatcher() {
         return (AbstractMatcher) super.getObject();
     }
