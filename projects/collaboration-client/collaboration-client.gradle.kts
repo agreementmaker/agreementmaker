033b4593c9fdf967d41ff88e46fe8e7edcdd9f69
Cosmin Stroe <cstroe@gmail.com>
2019-09-10 23:30:04 -0500

    Update jackson-databind dependency.

--- projects/collaboration-client/collaboration-client.gradle.kts
+++ projects/collaboration-client/collaboration-client.gradle.kts
@@ -10,5 +10,5 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.11.1")
 
     implementation("javax.ws.rs:jsr311-api:1.1.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9.2")
 }
